c26b0dae50d508afe429550fdafa1ce6270a2f67
KJeiii <pleasemailtokj@gmail.com>
2023-11-23 22:44:13 +0800

    comlete member system

---
+++ models/Member.py
@@ -0,0 +1,67 @@
+from mysql.connector import pooling
+from __init__ import db_config
+
+
+class MemberTool(pooling.MySQLConnectionPool):
+    def __init__(self):
+        super().__init__(pool_name = "RoadBuddy",
+                         pool_size = 10,
+                         pool_reset_session = True,
+                         **db_config)
+        
+    def Create_database(self) -> None:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+        create_string = ("create database roadbuddy")
+        cursor.execute(create_string)
+        connection.close()
+
+    def Create_table(self) -> None:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        create_string = ("create table member (" 
+                         "user_id bigint primary key auto_increment, "
+                         "username varchar(255) not null, "
+                         "email longtext not null, "
+                         "password longtext not null)"
+                         )
+ 
+        cursor.execute(create_string)
+        connection.close()       
+
+        
+    def Search_member(self, email: str) -> list:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        search_string = ('select * from member '
+                         'where email = %s'
+                        )
+        data_string = (email, )
+                
+        cursor.execute(search_string, data_string)
+        result = cursor.fetchall()
+        connection.close()
+
+        return result
+
+        
+    def Signup(self, user_name: str, email: str, password: str) -> None:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        update_string = (
+                        "insert into member (username, email, password)"
+                        "values (%s, %s, %s)"
+                        )
+        data_string = (user_name, email, password)
+                    
+        cursor.execute(update_string, data_string)
+        connection.commit()
+        connection.close()
+
+# test = MemberTool()
+# test.Create_database("roadbuddy")
+# test.Signup("mark", "mark@mail", "123")
+
