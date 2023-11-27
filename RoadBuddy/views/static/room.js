2034cef232cb1361307cc0e74b1b15e65db28bb7
KJeiii <pleasemailtokj@gmail.com>
2023-11-27 14:49:09 +0800

    complete friend/team switch

--- RoadBuddy/views/static/room.js
+++ RoadBuddy/views/static/room.js
@@ -13,69 +13,118 @@ function drawMap(position){
     let marker = L.marker([initialCoord.latitude, initialCoord.longitude]).addTo(map);
 };
 
-function userCoordError(error) {console.log(`Error in getting user position: ${error}`)};
 
-window.navigator.geolocation.getCurrentPosition(drawMap, userCoordError);
-
-
-
-async function loadMap(){
-    if (navigator.geolocation) {
-        try {
-            initialCoord = await window.navigator.geolocation.getCurrentPosition((position)=> {
-                console.log(position);
-                return [position.coords.latitude, position.coords.longitude];
-                });
-            console.log(initialCoord);
-            let map = L.map('map').setView([initialCoord[0], initialCoord[1]], 30);
-            const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);
-            let marker = L.marker([initialCoord[0], initialCoord[1]]).addTo(map);
-        }
-        catch(error) {
-            console.log(error)
-        }
-    }
-}
-
-
-// test user's position by select position from randomPosition
-let randomCoords = {
-    latitude: 24.982 + Math.random()*0.006,
-    longitude: 121.534 + Math.random()*0.006
-};
-
-// console.log(randomCoords);
-
-// let map = L.map('map').setView([randomCoords.latitude, randomCoords.longitude], 30);
-// const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//     }).addTo(map);
-// let marker = L.marker([randomCoords.latitude, randomCoords.longitude]).addTo(map);
+function userCoordError(error) {console.log(`Error in drawing initial map: ${error}`)};
 
 
 
-// if (navigator.geolocation) {
+// ----- switch menu -----
+let//
+menu = document.querySelector(".nav-menu"),
+menuTitle = document.querySelector(".nav-menu-title"),
+menuList = document.querySelector(".nav-menu-list"),
+toggleIcon = document.querySelector(".nav-toggle"),
+menuFriends = document.querySelector(".nav-menu-friend"),
+menuTeam = document.querySelector(".nav-menu-team"),
+friendsList = document.querySelector(".friends-list"),
+teamsList = document.querySelector(".teams-list"),
+addFriend = document.querySelector(".nav-add-friend"),
+addTeam = document.querySelector(".nav-add-team");
+
+toggleIcon.addEventListener("click", ()=>{
+    menu.style.border = "0.5px solid rgb(151, 150, 150)";
+    menuList.style.display = "block";
+
+})
+
+menuFriends.addEventListener("click", ()=>{
+    menuTitle.textContent = menuFriends.textContent;
+    menuList.style.display = "none";
+    menu.style.border = "none";
+    teamsList.style.display = "none";
+    addTeam.style.display = "none";
+    friendsList.style.display = "block";
+    addFriend.style.display = "block";
+    
+
+})
+
+menuTeam.addEventListener("click", ()=>{
+    menuTitle.textContent = menuTeam.textContent;
+    menuList.style.display = "none";
+    menu.style.border = "none";
+    friendsList.style.display = "none";
+    addFriend.style.display = "none";
+    teamsList.style.display = "block";
+    addTeam.style.display = "block";
+})
+
+
+
+// ----- add friend -----
+addFriend.addEventListener("click", () => {
+    console.log("add friends");
+})
+
+
+
+// ----- add team -----
+addTeam.addEventListener("click", () => {
+    console.log("add team")
+})
+
+
+
+
+
+
+// if (window.navigator.geolocation) {
 //     try {
-//         window.navigator.geolocation.getCurrentPosition((position)=> {
-//             console.log(position);
-//             initialCoord = [position.coords.latitude, position.coords.longitude];
-//             });
-        
-//         let map = L.map('map').setView([initialCoord[0], initialCoord[1]], 30);
-//         const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//             maxZoom: 19,
-//             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//             }).addTo(map);
-//         let marker = L.marker([initialCoord[0], initialCoord[1]]).addTo(map);
+//         window.navigator.geolocation.getCurrentPosition(drawMap, userCoordError);
+//         console.log("check in initialize map")
 //     }
-//     catch(error) {
-//         console.log(error)
-//     }
+//     catch(error) {console.log(`Error in getting user postion : ${error}`)}
 // }
+
+
+
+// function initializeMap() {
+//     return new Promise((resolve, reject) => {
+//         if (window.navigator.geolocation) {
+//             try {
+//                 window.navigator.geolocation.getCurrentPosition(drawMap, userCoordError);
+//                 console.log("check in initialize map")
+//                 resolve();
+//             }
+//             catch(error) {console.log(`Error in getting user postion : ${error}`)}
+//         }
+//     })
+// }
+
+
+// async function loadPage() {
+//     initializeMap()
+//         .then(() => {console.log("test")})
+//         .then(() => {
+//             console.log("test");
+//             // add pannel div
+//             let pannelDiv = document.createElement("div");
+//             pannelDiv.setAttribute("class", "pannel");
+//             document.querySelector(".background").append(pannelDiv);
+//         })
+//         .then(() => console.log("Done"))
+//         .catch((error) => console.log(error))
+// }
+
+// loadPage()
+
+
+
+
+
+
+
+
 
 
 
@@ -102,3 +151,10 @@ joinBtn.addEventListener("click", () => {
     sessionStorage.setItem("username", username);
     sessionStorage.setItem("roomID", roomID);
 });
+
+
+// test user's position by select position from randomPosition
+let randomCoords = {
+    latitude: 24.982 + Math.random()*0.006,
+    longitude: 121.534 + Math.random()*0.006
+};
