ca1a046ec3b249bb152cdcd463c8eb345323d9e4
KJeiii <pleasemailtokj@gmail.com>
2023-11-28 20:14:43 +0800

    add Delete_table function and rebuild friends table

--- RoadBuddy/models/friends.py
+++ RoadBuddy/models/friends.py
@@ -1,5 +1,6 @@
 from mysql.connector import pooling
-from RoadBuddy.models import db_config
+# from RoadBuddy.models import db_config
+from __init__ import db_config
 
 
 class FriendsTool(pooling.MySQLConnectionPool):
@@ -14,26 +15,52 @@ class FriendsTool(pooling.MySQLConnectionPool):
         cursor = connection.cursor(dictionary=True)
 
         create_string = ("create table friends (" 
-                         "user_id bigint primary key auto_increment, "
+                         "id bigint primary key auto_increment, "
+                         "user_id bigint not null, "
                          "friend_id bigint not null, "
+                         "foreign key(user_id) references member(user_id),"
                          "foreign key(friend_id) references member(user_id))"
                          )
  
         cursor.execute(create_string)
         connection.close() 
-        
 
-    def Add_friend(self, username:str) -> None:
-        pass
-
-
-    def Search_friends(self, username:str) -> list:
+    
+    def Delete_table(self) -> None:
         connection = self.get_connection()
         cursor = connection.cursor(dictionary=True)
 
-        search_string = ("select  from ")
-        pass
+        create_string = ("drop table friends")
+ 
+        cursor.execute(create_string)
+        connection.close() 
+        
 
-# test = FriendsTool().Create_table()
+    def Add_friend(self, user_id:int, friend_id:int) -> None:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        insert_string = ("insert into friends(user_id, friend_id) "
+                         "values (%s, %s)"
+                         )
+        data_string = (user_id, friend_id)
+
+        cursor.execute(insert_string, data_string)
+        connection.commit()
+        connection.close()
+
+
+    def Search_friends(self, user_id:int) -> list:
+        connection = self.get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        search_string = ("select * from friends "
+                         "where user_id = %s"
+                         )
+        data_string = (user_id,)
+
+        cursor.execute(search_string, data_string)
+        connection.close()
+
 
     
