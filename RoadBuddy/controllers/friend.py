65145d4adc2ce36b8e63919b574c59a70733803e
KJeiii <pleasemailtokj@gmail.com>
2023-11-28 21:55:59 +0800

    build api: Load_friends_list

--- RoadBuddy/controllers/friend.py
+++ RoadBuddy/controllers/friend.py
@@ -10,5 +10,23 @@ friends_bp = Blueprint("friends_bp",
 # Signup
 @friends_bp.route("/api/friends", methods = ["POST"])
 def Load_friends_list():
-    user_id = request.json["user_id"]
+    if request.method == "POST":
+        try:
+            user_id = request.json["user_id"]
+            friends_list = friendsTool.Load_friends_list(user_id)
+            response = {
+                "ok": True,
+                "data": friends_list 
+            }
+            return jsonify(response), 200
+        
+        except Exception as error:
+            print(f'Error in controller(friend) - Load_friends_list : {error}')
+            response = {
+                "error": True,
+                "message": "伺服器內部錯誤"
+            }
+            return jsonify(response), 500
     
+
+
