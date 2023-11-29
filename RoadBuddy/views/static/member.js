1f3a0c7db8237e361e598107afe633ed3f13be57
KJeiii <pleasemailtokj@gmail.com>
2023-11-30 07:56:13 +0800

    member page: redirect to room page if jwt exists

--- RoadBuddy/views/static/member.js
+++ RoadBuddy/views/static/member.js
@@ -1,3 +1,38 @@
+// build function for checking user status
+async function CheckUserStatus() {
+    let jwt = window.localStorage.getItem("token");
+    console.log(jwt);
+
+    try{
+        if ( jwt === null) {return {"ok":false, "data": null}}
+
+        let//
+        response = await fetch("/api/member/auth", {
+            method: "GET",
+            headers: {"authorization": `Bearer ${jwt}`}
+        }),
+        result = await response.json();
+
+        if (result.data === null) {return {"ok": false, "data": null}}
+
+        let data = {
+            user_id: result.user_id,
+            username: result.username,
+            email: result.email
+        }
+        return {"ok": true, "data": data}
+    }
+    catch(error) {
+        console.log(`Error in CheckUserStatus : ${error}`)
+        throw error
+    }
+}
+
+CheckUserStatus()
+    .then((result) => {window.location.replace("/room")})
+    .catch((error) => {console.log(`Error from CheckUserStatus in member page : ${error}`)})
+
+    
 // ----- switch sigup/login page -----
 let//
 signupDiv = document.querySelector(".title-signup"),
