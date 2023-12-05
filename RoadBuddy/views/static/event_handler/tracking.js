dc019140e35d5552770fa3016976b3871d2e0a6e
KJeiii <pleasemailtokj@gmail.com>
2023-12-05 17:07:54 +0800

    fix js object parsing property bug : use [] to specify key

--- RoadBuddy/views/static/event_handler/tracking.js
+++ RoadBuddy/views/static/event_handler/tracking.js
@@ -7,33 +7,23 @@ let randomCoords = {
 
 
 
-// ----- listener for receiving "join_team" event from server ----- 
-// ----- and send user initial position to socket -----
-socket.on("enter_team", () => {
-    let data = {
-        sid : sessionStorage.getItem("sid"),
-        user_id : sessionStorage.getItem("user_id"),
-        username : sessionStorage.getItem("username"),
-        email : sessionStorage.getItem("email"),
-        team_id : sessionStorage.getItem("team_id"),
-        coord : {
-            latitude: initialCoord[0],
-            longitude: initialCoord[1]
-        }
-    };
-
-    socket.emit("position", data);
-})
-
-
-
 // ----- receive partners initial postion and show on the map -----
 socket.on("initPosition", (partners) => {
+    let sid = socket.id;
+    console.log(sid);
+    console.log(typeof(Object.keys(partners)[0]));
+    console.log(sid === Object.keys(partners)[0]);
+    console.log(partners);
+    console.log(partners[Object.keys(partners)[0]]);
+    console.log(partners[sid]);
+
+
     for ( sid in partners ) {
         if ( sid !== socket.id && !sidArray.includes(sid)) {
             let markerToAdd = L.marker([partners[sid][0].latitude, partners[sid][0].longitude]).addTo(map);
             sidArray.push(sid);
             markerArray.push(markerToAdd);
+            console.log(`${sid} coords is ${partners[sid][0].latitude}, ${partners[sid][0].longitude}`)
         }
     }
 });
@@ -75,14 +65,43 @@ setInterval(()=> {
     };
 
     let data = {
-        roomID: sessionStorage.getItem("roomID"),
-        coord: {
+        sid : sessionStorage.getItem("sid"),
+        user_id : sessionStorage.getItem("user_id"),
+        username : sessionStorage.getItem("username"),
+        email : sessionStorage.getItem("email"),
+        team_id : sessionStorage.getItem("team_id"),
+        coord : {
             latitude: randomCoords.latitude,
-            longitude: randomCoords.longitude            
+            longitude: randomCoords.longitude    
         }
     };
+    socket.emit("position", data);
     console.log(`new data sent from client ${socket.id} : ${data}`);
 
-    socket.emit("position", data);
 }, 2000)
 
+
+// var turnOnTracking = false;
+// while (turnOnTracking) {
+//     setInterval(()=> {
+//         let randomCoords = {
+//             latitude: 24.982 + Math.random()*0.006,
+//             longitude: 121.534 + Math.random()*0.006
+//         };
+
+//         let data = {
+//             sid : sessionStorage.getItem("sid"),
+//             user_id : sessionStorage.getItem("user_id"),
+//             username : sessionStorage.getItem("username"),
+//             email : sessionStorage.getItem("email"),
+//             team_id : sessionStorage.getItem("team_id"),
+//             coord : {
+//                 latitude: randomCoords.latitude,
+//                 longitude: randomCoords.longitude    
+//             }
+//         };
+//         socket.emit("position", data);
+//         console.log(`new data sent from client ${socket.id} : ${data}`);
+
+//     }, 2000)
+// }
