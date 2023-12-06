0d038f0bd3de488d29bc2b7ad905827a1c813cd0
KJeiii <pleasemailtokj@gmail.com>
2023-12-06 21:49:27 +0800

    user browser navigator.geolocation.watchPosition method to parse user position

--- RoadBuddy/views/static/event_handler/tracking.js
+++ RoadBuddy/views/static/event_handler/tracking.js
@@ -10,21 +10,64 @@ let randomCoords = {
 // ----- receive partners initial postion and show on the map -----
 socket.on("initPosition", (partners) => {
     let colorReference = (team_sender_info_cache === undefined) ? partnersColor : team_sender_info_cache.friends_color;
+    console.log(markerArray);
+    console.log(partners);
+    console.log(Object.keys(partners).length >= 2);
 
-    for ( sid in partners ) {
-        let id = sidReference[sid];
-        if ( sid !== socket.id && !sidArray.includes(sid)) {
-            // add circleMarker
-            let markerOption = {
-                color: colorReference[id].color
-            };
-            let markerToAdd = L.circleMarker([partners[sid][0].latitude, partners[sid][0].longitude], markerOption).addTo(map);
+    if (Object.keys(partners).length >= 2) {
+        // update new own circleMarker with partners information
+        console.log(markerArray);
 
-            // register new partner information (sid, circleMaker object)
-            sidArray.push(sid);
-            markerArray.push(markerToAdd);
+        for ( let i = 0; i < markerArray.length; i++) {
+            map.removeLayer(markerArray[i]);
+            markerArray.pop();
         }
+        
+        let//
+        id = sidReference[socket.id],
+        markerOption = {
+            color: colorReference[id].color,
+            fillOpacity: 0.7
+        },
+        markerToAdd = L.circleMarker([partners[socket.id][0].latitude, partners[socket.id][0].longitude], markerOption).addTo(map);
+        markerArray.push(markerToAdd);
+
+        // update other partners circleMarker
+        for ( sid in partners ) {
+            let id = sidReference[sid];
+            if ( sid !== socket.id && !sidArray.includes(sid)) {
+                // add circleMarker
+                let markerOption = {
+                    color: colorReference[id].color,
+                    fillOpacity: 0.7
+                };
+                let markerToAdd = L.circleMarker([partners[sid][0].latitude, partners[sid][0].longitude], markerOption).addTo(map);
+    
+                // register new partner information (sid, circleMaker object)
+                sidArray.push(sid);
+                markerArray.push(markerToAdd);
+            }
+        }
+        return
     }
+    console.log(`markerArray after add new partner : ${markerArray}`);
+
+    // stay on room page, not joining team
+    // for ( sid in partners ) {
+    //     let id = sidReference[sid];
+    //     if ( sid !== socket.id && !sidArray.includes(sid)) {
+    //         // add circleMarker
+    //         let markerOption = {
+    //             color: colorReference[id].color,
+    //             fillOpacity: 0.7
+    //         };
+    //         let markerToAdd = L.circleMarker([partners[sid][0].latitude, partners[sid][0].longitude], markerOption).addTo(map);
+
+    //         // register new partner information (sid, circleMaker object)
+    //         sidArray.push(sid);
+    //         markerArray.push(markerToAdd);
+    //     }
+    // }
 });
 
 
@@ -37,7 +80,7 @@ socket.on("movingPostion", (partners) => {
         let//
         oldLatLng = [partners[sid][0].latitude, partners[sid][0].longitude],
         newLatLng = [partners[sid][1].latitude, partners[sid][1].longitude],
-        movingMarker = markerArray[sidArray.indexOf(sid)+1];
+        movingMarker = markerArray[sidArray.indexOf(sid)];
 
         movingMarker.setLatLng(oldLatLng, newLatLng);
         }
@@ -57,14 +100,19 @@ socket.on("message", (msg) => {
 });
 
 
+// ----- tracking user device position changing-----
+let coordFromBrowser = {};
+let watchCoord = window.navigator.geolocation.watchPosition(
+    (position) => {
+        coordFromBrowser = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+    }
+);
 
-// ----- change postion randomly -----
-setInterval(()=> {
-    let randomCoords = {
-        latitude: 24.982 + Math.random()*0.006,
-        longitude: 121.534 + Math.random()*0.006
-    };
-
+setInterval(() => {
+    console.log(coordFromBrowser);
     let data = {
         sid : sessionStorage.getItem("sid"),
         user_id : sessionStorage.getItem("user_id"),
@@ -72,10 +120,34 @@ setInterval(()=> {
         email : sessionStorage.getItem("email"),
         team_id : sessionStorage.getItem("team_id"),
         coord : {
-            latitude: randomCoords.latitude,
-            longitude: randomCoords.longitude    
+            latitude: coordFromBrowser.latitude,
+            longitude: coordFromBrowser.longitude    
         }
     };
+    
     socket.emit("position", data);
-    console.log(`new coord from client ${socket.id} : ${randomCoords.latitude}, ${randomCoords.longitude}`);
-}, 2000)
+
+}, 2000);
+
+
+
+// ----- change postion randomly -----
+// setInterval(()=> {
+//     let randomCoords = {
+//         latitude: 24.982 + Math.random()*0.006,
+//         longitude: 121.534 + Math.random()*0.006
+//     };
+
+//     let data = {
+//         sid : sessionStorage.getItem("sid"),
+//         user_id : sessionStorage.getItem("user_id"),
+//         username : sessionStorage.getItem("username"),
+//         email : sessionStorage.getItem("email"),
+//         team_id : sessionStorage.getItem("team_id"),
+//         coord : {
+//             latitude: randomCoords.latitude,
+//             longitude: randomCoords.longitude    
+//         }
+//     };
+//     socket.emit("position", data);
+// }, 2000)
