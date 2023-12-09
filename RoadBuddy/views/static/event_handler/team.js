5c0861cc54c0fd00e5ed5589ba4dc32295a5e9d4
KJeiii <pleasemailtokj@gmail.com>
2023-12-09 10:10:09 +0800

    change parters list item creation: partner item appears when it join the team

--- RoadBuddy/views/static/event_handler/team.js
+++ RoadBuddy/views/static/event_handler/team.js
@@ -40,22 +40,26 @@ startTripBtn.addEventListener("click", ()=> {
     // create partner information in partners-list
     let partnersList = document.querySelector(".tracking-pannel .partners-list");
     for ( id in partnersColor) {
-        let//
-        item = document.createElement("div"),
-        icon = document.createElement("div"),
-        username = document.createElement("div");
+        if (id*1 === window.sessionStorage.getItem("user_id")*1) {
+            let//
+            item = document.createElement("div"),
+            icon = document.createElement("div"),
+            username = document.createElement("div");
+        
+            item.setAttribute("class", "item");
+            item.setAttribute("id", id);
+            icon.setAttribute("class", "icon");
+            icon.style.backgroundColor = partnersColor[id].color;
+            username.setAttribute("class", "username");
+            username.setAttribute("id", id);
+            username.textContent = partnersColor[id].username;
     
-        item.setAttribute("class", "item");
-        icon.setAttribute("class", "icon");
-        icon.style.backgroundColor = partnersColor[id].color;
-        username.setAttribute("class", "username");
-        username.setAttribute("id", id);
-        username.textContent = partnersColor[id].username;
-
-        item.appendChild(icon);
-        item.appendChild(username);
-        partnersList.appendChild(item);
+            item.appendChild(icon);
+            item.appendChild(username);
+            partnersList.appendChild(item);
+        }
     }
+
 
 
     // send request for joining team
@@ -84,10 +88,9 @@ startTripBtn.addEventListener("click", ()=> {
 })
 
 
-// ----- listener to event "team_request" -----
+// ----- listener for receiving event "team_request" from server -----
 var team_sender_info_cache;
 socket.on("team_request", (data) => {
-    console.log(`Receive team request from ${data.username}`);
     team_sender_info_cache = data
 
     // prompt to ask willness
@@ -104,7 +107,6 @@ socket.on("team_request", (data) => {
 // if accept 
 let teamYesBtn = document.querySelector(".team-invite-prompt .yes");
 teamYesBtn.addEventListener("click", () => {
-    console.log("click")
     // switch to tracking pannel
     mainPannel.style.display = "none";
     friendsPannel.style.display = "none";
@@ -113,23 +115,44 @@ teamYesBtn.addEventListener("click", () => {
     
     // create partner information in partners-list
     let partnersList = document.querySelector(".tracking-pannel .partners-list");
-    console.log(team_sender_info_cache.friends_color);
     for ( id in team_sender_info_cache.friends_color) {
-        let//
-        item = document.createElement("div"),
-        icon = document.createElement("div"),
-        username = document.createElement("div");
+        if ( id*1 === team_sender_info_cache["user_id"]*1 ) {
+            let//
+            item = document.createElement("div"),
+            icon = document.createElement("div"),
+            username = document.createElement("div");
+        
+            item.setAttribute("class", "item");
+            item.setAttribute("id", id);
+            icon.setAttribute("class", "icon");
+            icon.style.backgroundColor = team_sender_info_cache.friends_color[id].color;
+            username.setAttribute("class", "username");
+            username.setAttribute("id", id);
+            username.textContent = team_sender_info_cache.friends_color[id].username;
     
-        item.setAttribute("class", "item");
-        icon.setAttribute("class", "icon");
-        icon.style.backgroundColor = team_sender_info_cache.friends_color[id].color;
-        username.setAttribute("class", "username");
-        username.setAttribute("id", id);
-        username.textContent = team_sender_info_cache.friends_color[id].username;
+            item.appendChild(icon);
+            item.appendChild(username);
+            partnersList.appendChild(item);
+        }
 
-        item.appendChild(icon);
-        item.appendChild(username);
-        partnersList.appendChild(item);
+        if ( id*1 === window.sessionStorage.getItem("user_id")*1 ) {
+            let//
+            item = document.createElement("div"),
+            icon = document.createElement("div"),
+            username = document.createElement("div");
+        
+            item.setAttribute("class", "item");
+            item.setAttribute("id", id);
+            icon.setAttribute("class", "icon");
+            icon.style.backgroundColor = team_sender_info_cache.friends_color[id].color;
+            username.setAttribute("class", "username");
+            username.setAttribute("id", id);
+            username.textContent = team_sender_info_cache.friends_color[id].username;
+    
+            item.appendChild(icon);
+            item.appendChild(username);
+            partnersList.appendChild(item);
+        }
     }
 
     // change elements in setting div
@@ -180,7 +203,6 @@ teamNoBtn.addEventListener("click", () => {
     };
 
     socket.emit("enter_team", joinTeamData);
-    console.log(`Reject response emitted to server`);
 
     // show response
     let//
@@ -211,7 +233,6 @@ teamOkBtn.addEventListener("click", ()=>{
 // ----- and emit user initial position to listener "position" on server -----
 socket.on("enter_team", (sid_reference) => {
     sidReference = sid_reference;
-    console.log(`type of team_id in browser session : ${typeof(sessionStorage.getItem("team_id"))}`);
     let data = {
         sid : sessionStorage.getItem("sid"),
         user_id : sessionStorage.getItem("user_id"),
@@ -224,7 +245,6 @@ socket.on("enter_team", (sid_reference) => {
         }
     };
 
-    console.log(`initialCoord in listener "enter_team on client ${data.coord.latitude}, ${data.coord.longitude}`);
     socket.emit("position", data);
 })
 
@@ -240,76 +260,157 @@ leaveTeamBtn.addEventListener("click", ()=> {
         email: window.sessionStorage.getItem("email")
     };
     socket.emit("leave_team", data);
-    window.sessionStorage.removeItem("team_id");
 
     // change elements in setting div
+    leaveTeamBtn.style.display = "none";
     settingOnMain.style.display = "block";
     settingOffMain.style.display = "none";
     settingOnTracking.style.display = "none";
     settingOffTracking.style.display = "none";
 
+    // remove all partner in the tracking pannel
+    let trackingPannelPartnerList = document.querySelector(".tracking-pannel .partners-list");
+    while (trackingPannelPartnerList.hasChildNodes()) {
+        trackingPannelPartnerList.removeChild(trackingPannelPartnerList.lastChild)
+    }
+
+    trackingPannel.style.display = "none";
+    teamsPannel.style.display = "none"
+    mainPannel.style.display = "block"
 })
+
+
 
 socket.on("leave_team", (data) => {
     let sid = data.sid;
-    markerArray.splice(sidArray.indexOf(sid),1);
-    sidArray.splice(sidArray.indexOf(sid),1);
-    window.sessionStorage.removeItem("team_id");
+    if ( socket.id === sid ) {
+        markerArray.splice(sidArray.indexOf(sid),1);
+        sidArray.splice(sidArray.indexOf(sid),1);
+        window.sessionStorage.removeItem("team_id");
+    }
+})
+
+
+//  ----- add new partners if they join -----
+socket.on("add_partner", (user_id) => {
+    let partnersList = document.querySelector(".tracking-pannel .partners-list");
+    // as a team owner
+    if ( team_sender_info_cache === undefined ) {
+        for ( id in partnersColor) {
+            if ( id*1 === user_id*1 ) {
+                let//
+                item = document.createElement("div"),
+                icon = document.createElement("div"),
+                username = document.createElement("div");
+            
+                item.setAttribute("class", "item");
+                item.setAttribute("id", id);
+                icon.setAttribute("class", "icon");
+                icon.style.backgroundColor = partnersColor[id].color;
+                username.setAttribute("class", "username");
+                username.setAttribute("id", id);
+                username.textContent = partnersColor[id].username;
+        
+                item.appendChild(icon);
+                item.appendChild(username);
+                partnersList.appendChild(item);
+            }
+        }
+        return
+    }
+
+    // as a partner
+    for ( id in team_sender_info_cache.friends_color) {
+        if ( id*1 === user_id*1 && id*1 !== team_sender_info_cache.user_id*1 && id*1 !== window.sessionStorage.getItem("user_id")*1) {
+            let//
+            item = document.createElement("div"),
+            icon = document.createElement("div"),
+            username = document.createElement("div");
+        
+            item.setAttribute("class", "item");
+            item.setAttribute("id", id);
+            icon.setAttribute("class", "icon");
+            icon.style.backgroundColor = team_sender_info_cache.friends_color[id].color;
+            username.setAttribute("class", "username");
+            username.setAttribute("id", id);
+            username.textContent = team_sender_info_cache.friends_color[id].username;
+    
+            item.appendChild(icon);
+            item.appendChild(username);
+            partnersList.appendChild(item);
+        }
+    }
+})
+
+
+// remove partners when they leave
+socket.on("remove_partner", (leaving_partner_data) => {
+    let user_id = leaving_partner_data["user_id"]*1;
+
+    let//
+    partnersList = document.querySelector(".tracking-pannel .partners-list");
+    partnerItems = document.querySelectorAll(".tracking-pannel .partners-list .item");
+
+    for ( item of partnerItems ) {
+        if ( item.getAttribute("id")*1 === user_id ) {
+            partnersList.removeChild(item);
+        }
+    }
 })
 
 
 // -------- msg test for room -------
 // test leave team for alert message
-let msgleaveTeamBtn = document.querySelector("div.alert button.leave");
-msgleaveTeamBtn.addEventListener("click", ()=> {
-    // switch to main pannel
-    friendsPannel.style.display = "none";
-    teamsPannel.style.display = "none";
-    trackingPannel.style.display = "none";
-    mainPannel.style.display = "none";
+// let msgleaveTeamBtn = document.querySelector("div.alert button.leave");
+// msgleaveTeamBtn.addEventListener("click", ()=> {
+//     // switch to main pannel
+//     friendsPannel.style.display = "none";
+//     teamsPannel.style.display = "none";
+//     trackingPannel.style.display = "none";
+//     mainPannel.style.display = "none";
 
-    // change elements in setting div
-    config.style.display = "none";
-    logout.style.display = "none";
-    leave.style.display = "none";
+//     // change elements in setting div
+//     config.style.display = "none";
+//     logout.style.display = "none";
+//     leave.style.display = "none";
 
-    // organize data for emitted to event listener "leave_team" on server
-    let data = {
-        team_id: `${window.sessionStorage.getItem("team_id")}`,
-        username: window.sessionStorage.getItem("username"),
-        user_id: window.sessionStorage.getItem("user_id"),
-        email: window.sessionStorage.getItem("email")
-    };
-    socket.emit("leave_team", data);
-    window.sessionStorage.removeItem("team_id");
-})
+//     // organize data for emitted to event listener "leave_team" on server
+//     let data = {
+//         team_id: `${window.sessionStorage.getItem("team_id")}`,
+//         username: window.sessionStorage.getItem("username"),
+//         user_id: window.sessionStorage.getItem("user_id"),
+//         email: window.sessionStorage.getItem("email")
+//     };
+//     socket.emit("leave_team", data);
+//     window.sessionStorage.removeItem("team_id");
+// })
 
 
-// test alert msg emitted to room
-let alertBtn = document.querySelector("button.alert");
-alertBtn.addEventListener("click", ()=>{
-    let//
-    msg = document.querySelector("div.alert input").value,
-    data = {
-        team_id: `${window.sessionStorage.getItem("team_id")}`,
-        msg: msg,
-        user_id: window.sessionStorage.getItem('user_id'),
-        username: window.sessionStorage.getItem('username'),
-        email: window.sessionStorage.getItem('email'),
-    };
+// // test alert msg emitted to room
+// let alertBtn = document.querySelector("button.alert");
+// alertBtn.addEventListener("click", ()=>{
+//     let//
+//     msg = document.querySelector("div.alert input").value,
+//     data = {
+//         team_id: `${window.sessionStorage.getItem("team_id")}`,
+//         msg: msg,
+//         user_id: window.sessionStorage.getItem('user_id'),
+//         username: window.sessionStorage.getItem('username'),
+//         email: window.sessionStorage.getItem('email'),
+//     };
 
-    socket.emit("alert", data);
-    console.log(`${window.sessionStorage.getItem("username")} send ${msg}`);
-})
+//     socket.emit("alert", data);
+//     console.log(`${window.sessionStorage.getItem("username")} send ${msg}`);
+// })
 
-socket.on("alert", (data)=>{
-    console.log(`Receive ${data.msg} from ${data.username}`);
+// socket.on("alert", (data)=>{
+//     console.log(`Receive ${data.msg} from ${data.username}`);
 
-    let//
-    msg = data.msg,
-    alert = document.querySelector("div.alert"),
-    msgDiv = document.createElement("div");
+//     let//
+//     msg = data.msg,
+//     alert = document.querySelector("div.alert"),
+//     msgDiv = document.createElement("div");
 
-    msgDiv.textContent = msg;
-    alert.appendChild(msgDiv);
-})
+//     msgDiv.textContent = msg;
+//     alert.appendChild(msgDiv);
+// })
