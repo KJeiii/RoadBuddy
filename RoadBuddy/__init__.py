bae1e527a911554d34715e1bce5bfc96e0618ab4
KJeiii <pleasemailtokj@gmail.com>
2023-12-09 10:02:58 +0800

    comment logger=True

--- RoadBuddy/__init__.py
+++ RoadBuddy/__init__.py
@@ -12,7 +12,7 @@ app.secret_key = "3b62657d32897eb69f59c089f0950dbe1ce4fd13"
 app.json.ensure_ascii = False
 
 socketio = SocketIO(app, 
-                    logger=True, 
+                    # logger=True, 
                     cors_allowed_origins="*", 
                     ping_timeout=60,
                     ping_interval=5
