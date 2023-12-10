b287f8703665f281c4a78901a8de61a7a2e4c039
KJeiii <pleasemailtokj@gmail.com>
2023-12-10 22:53:57 +0800

    register initial_friend_status and online_friend_status event listeners

--- RoadBuddy/event_handler/friend.py
+++ RoadBuddy/event_handler/friend.py
@@ -43,6 +43,71 @@ def friend_request_result(data):
     emit("friend_request_result", data, to=sender_sid)
 
 
+@socketio.on("initial_friend_status")
+def initial_friend_status():
+    print(f'receive {user_info[sid_reference[request.sid]]["username"]} initial online status')
+    # get online friends data for user first time login
+    user_id = sid_reference[request.sid]
+    friend_list = user_info[user_id]["friend_list"]
+    online_friend_info = {}
+    
+    for friend in friend_list:
+        friend_id = int(friend["user_id"])
+        if friend_id in user_info.keys():
+            online_friend_info[friend_id] = friend["username"]
+
+    emit("update_friend_status", online_friend_info, to=request.sid)
+
+
+@socketio.on("online_friend_status")
+def online_friend_status():
+    print(f'receive {user_info[sid_reference[request.sid]]["username"]} update online status')
+    # send "online-status" event to friends on-line
+    user_id = sid_reference[request.sid]
+    friend_list = user_info[user_id]["friend_list"]
+    friend_sid_online = []
+    for friend in friend_list:
+        friend_id = int(friend["user_id"])
+        if friend_id in user_info.keys():
+            friend_sid = user_info[friend_id]["sid"]
+            friend_sid_online.append(friend_sid)
+
+    my_user_info = {
+        "user_id": user_id,
+        "username": user_info[user_id]["username"],
+        "sid": request.sid,
+        "email": user_info[user_id]["email"]
+    }
+
+    for sid in friend_sid_online:
+        emit("update_friend_status", my_user_info, to=sid)
+
+
+# @socketio.on("offline_friend_status")
+# def offline_friend_status():
+#     print(f'receive {user_info[sid_reference[request.sid]]["username"]} update offline status')
+#     # send "online-status" event to friends on-line
+#     user_id = sid_reference[request.sid]
+#     friend_list = user_info[user_id]["friend_list"]
+#     friend_sid_online = []
+#     for friend in friend_list:
+#         friend_id = int(friend["user_id"])
+#         if friend_id in user_info.keys():
+#             friend_sid = user_info[friend_id]["sid"]
+#             friend_sid_online.append(friend_sid)
+
+#     my_user_info = {
+#         "user_id": user_id,
+#         "username": user_info[user_id]["username"],
+#         "sid": request.sid,
+#         "email": user_info[user_id]["email"]
+#     }
+
+#     for sid in friend_sid_online:
+#         emit("offline_friend_status", my_user_info, to=sid)
+
+
+
 @socketio.on("alert")
 def alert(data):
     emit("alert", data, to=data["receiver_sid"])
