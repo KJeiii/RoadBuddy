dd18071933425f414161360f9f3df16c29baa73a
KJeiii <pleasemailtokj@gmail.com>
2023-12-30 15:24:31 +0800

    build event listner for new friend requesting for joining team

--- RoadBuddy/event_handler/team.py
+++ RoadBuddy/event_handler/team.py
@@ -5,8 +5,8 @@ from RoadBuddy.event_handler import sid_reference, user_info, rooms_info
 
 
 # Listener for receiving event "team request" from server
-@socketio.on("team_request")
-def team_request(data):
+@socketio.on("team_invite")
+def team_invite(data):
     sender_sid = data["sender_sid"]
     sender_id = sid_reference[sender_sid]
 
@@ -17,9 +17,9 @@ def team_request(data):
             "username": user_info[sender_id]["username"],
             "email": user_info[sender_id]["email"],
             "team_id": data["team_id"],
-            "friends_color": data["receiver_info"]["receiver_color"]
+            "partners_color": data["receiver_info"]["receiver_color"]
         }
-        emit("team_request", sender_info, to=user_info[id]["sid"])
+        emit("team_invite", sender_info, to=user_info[id]["sid"])
 
 
 
@@ -30,18 +30,18 @@ def enter_team(data):
     sender_id = sid_reference[sender_sid]
     user_sid = request.sid
     user_id = sid_reference[user_sid]
-    team_id = data["team_id"]
 
     if data["accept"]:
         # team owner create team
         if data["enter_type"] == "create":
+            team_id = data["team_id"]
             if team_id not in rooms_info.keys():
                 # rooms_info[team_id] = {}
                 # rooms_info[team_id][request.sid] = []
 
                 rooms_info[team_id] = {
                     "owner_sid": request.sid,
-                    "partner": {}
+                    "partner": {request.sid : []}
                 }
 
                 join_room(team_id)
@@ -51,10 +51,14 @@ def enter_team(data):
             else:
                 print(f'{team_id} is in used')
 
-        # partner join team
+        # partner join team by owner invitation
         if data["enter_type"] == "join":
-            if team_id in rooms_info.keys() and request.sid in data["receiver_sid"]:
-                rooms_info[team_id][data["receiver_sid"]] = []
+            team_id = data["team_id"]
+            # if team_id in rooms_info.keys() and request.sid in data["receiver_sid"]:
+            # -> seems uneccesary to check request.sid in data["receiver_sid"], it must be true
+             
+            if team_id in rooms_info.keys():
+                rooms_info[team_id]["partner"][request.sid] = []
                 join_room(team_id)
                 user_info[user_id]["team_id"] = team_id
                 emit("enter_team", sid_reference, to=team_id)
@@ -62,6 +66,26 @@ def enter_team(data):
 
             else:
                 print(f'{team_id} has not created by owner yet')
+
+        # owner accept request from new partner
+        # if data["enter_type"] == "accept_request":
+        #     team_id = user_info[sid_reference[request.sid]]["team_id"]
+
+        #     info_for_new_partner = {
+        #         "sender_info": {        
+        #             "sid": request.sid,
+        #             "user_id": sid_reference[request.sid],
+        #             "username": user_info[sid_reference[request.sid]]["username"],
+        #             "email": user_info[sid_reference[request.sid]]["email"],
+        #             "team_id": team_id,
+        #             "partners_color": data["partners_color"]
+        #         },
+        #         "sid_reference": sid_reference,
+        #         "requester_info": data["requester_info"]
+        #      }
+            
+        #     emit("enter_team", info_for_new_partner, to=team_id)
+        #     emit("add_partner", data["requester_info"]["user_id"], to=team_id)
 
 
 
@@ -77,10 +101,10 @@ def leave_team(data):
     emit("remove_partner", data, to=team_id)
 
     leave_room(team_id)
-    del rooms_info[team_id][sid]
+    del rooms_info[team_id]["partner"][sid]
     del user_info[user_id]["team_id"]
 
-    if len(rooms_info[team_id].keys()) <= 0:
+    if len(rooms_info[team_id]["partner"].keys()) <= 0:
         del rooms_info[team_id]
         team_online_list = list(rooms_info.keys())
         emit("update_team_status", team_online_list, broadcast=True)
@@ -109,11 +133,29 @@ def update_team_status():
 
 
 # Event listener for receiving event "join_team_request" from frontend
+# And emit event "join_team_request" to team owner in frontend 
 @socketio.on("join_team_request")
 def join_team_request(data):
-    if data["team_id_to_join"] in rooms_info.keys():
-        
+    if data["team_id"] in rooms_info.keys():
+        team_owner_sid = rooms_info[data["team_id"]]["owner_sid"]
+        emit("join_team_request", data, to=team_owner_sid)
 
+
+@socketio.on("accept_team_request")
+def accept_team_request(data):
+    if data["accept"]:
+        requester_sid = data["requester_info"]["user_sid"]
+        sender_info = {
+            "sid": request.sid,
+            "user_id": sid_reference[request.sid],
+            "username": user_info[sid_reference[request.sid]]["username"],
+            "email": user_info[sid_reference[request.sid]]["email"],
+            "team_id": user_info[sid_reference[request.sid]]["team_id"],
+            "partners_color": data["partners_color"]
+        }
+        
+        emit("accept_team_request", sender_info, to=requester_sid)
+        
 
 
 
