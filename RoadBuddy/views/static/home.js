33537b399f34105f3a656e1fe901d47e18d1700b
KJeiii <pleasemailtokj@gmail.com>
2024-02-03 17:52:19 +0800

    ask for tracking location at home page

--- RoadBuddy/views/static/home.js
+++ RoadBuddy/views/static/home.js
@@ -2,3 +2,14 @@ let startBtn = document.querySelector(".start");
 startBtn.addEventListener("click", () => {
     window.location.replace("/member")
 });
+
+// ----- tracking user device position changing-----
+let coordFromBrowser = {};
+let watchCoord = window.navigator.geolocation.watchPosition(
+    (position) => {
+        coordFromBrowser = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+    }
+);
