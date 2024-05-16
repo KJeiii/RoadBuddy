f64ee96fbececa18ada0f997f5ed85dcde1bec14
KJeiii <pleasemailtokj@gmail.com>
2024-05-16 17:14:27 +0800

    format code

--- RoadBuddy/views/static/Utils/GeneralControl.js
+++ RoadBuddy/views/static/Utils/GeneralControl.js
@@ -1,6 +1,6 @@
 import * as DOMElements from "./DOMElements.js";
 
-export function switchToTrackingPannel () {
+export function switchToTrackingPannel() {
     // switch to tracking pannel
     DOMElements.mainPannel.style.display = "none";
     DOMElements.friendsPannel.style.display = "none";
@@ -17,27 +17,27 @@ export function switchToTrackingPannel () {
 
 export function switchPannel(toPannel, ...turnOnBtns) {
     let//
-    pannels = [
-        DOMElements.mainPannel, 
-        DOMElements.friendsPannel, 
-        DOMElements.teamsPannel,
-        DOMElements.trackingPannel
-    ],
-    btns = [
-        DOMElements.settingOnMain, 
-        DOMElements.settingOffMain, 
-        DOMElements.settingOnTracking, 
-        DOMElements.settingOffTracking, 
-        DOMElements.logout,
-        DOMElements.invite,
-        DOMElements.leave,
-        DOMElements.pullUpFriend,
-        DOMElements.pullUpTeam,
-        DOMElements.pullUpTracking,
-        DOMElements.dropDownFriend,
-        DOMElements.dropDownTeam,
-        DOMElements.dropDownTracking   
-    ];
+        pannels = [
+            DOMElements.mainPannel,
+            DOMElements.friendsPannel,
+            DOMElements.teamsPannel,
+            DOMElements.trackingPannel
+        ],
+        btns = [
+            DOMElements.settingOnMain,
+            DOMElements.settingOffMain,
+            DOMElements.settingOnTracking,
+            DOMElements.settingOffTracking,
+            DOMElements.logout,
+            DOMElements.invite,
+            DOMElements.leave,
+            DOMElements.pullUpFriend,
+            DOMElements.pullUpTeam,
+            DOMElements.pullUpTracking,
+            DOMElements.dropDownFriend,
+            DOMElements.dropDownTeam,
+            DOMElements.dropDownTracking
+        ];
 
     // turn on specified pannel; others would be turned off
     for (let pannel of pannels) {
@@ -58,43 +58,44 @@ export function switchPannel(toPannel, ...turnOnBtns) {
     }
 }
 
-export function ControlMsgBox(msgCssSelector, display, ...rest){
+export function ControlMsgBox(msgCssSelector, display, ...rest) {
     if (display === "none") {
         let//
-        msgBox = document.querySelector(msgCssSelector),
-        msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
+            msgBox = document.querySelector(msgCssSelector),
+            msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
 
         msgBoxContent.textContent = "";
         msgBox.style.display = display;
         return
     }
 
-    if (msgCssSelector === ".friend-prompt" && display === "block"){
+    if (msgCssSelector === ".friend-prompt" && display === "block") {
         let//
-        msgBox = document.querySelector(msgCssSelector),
-        msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
+            msgBox = document.querySelector(msgCssSelector),
+            msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
         msgBoxContent.textContent = `來自 ${rest[0].friendName} 的好友邀請`;
         msgBox.style.display = display;
         return
     }
 
-    if (msgCssSelector === ".friend-request" && display === "block"){
+    if (msgCssSelector === ".friend-request" && display === "block") {
         let//
-        msgBox = document.querySelector(msgCssSelector),
-        msgBoxContent = document.querySelector(`${msgCssSelector} .content`),
-        statement = `已發出交友申請`;
+            msgBox = document.querySelector(msgCssSelector),
+            msgBoxContent = document.querySelector(`${msgCssSelector} .content`),
+            statement = `已發出交友申請`;
 
         // Not allowed if no one is selected
-        if (rest[0].selectedFriendIDs.length === 0) {statement = `你尚未選擇對象`}
+        if (rest[0].selectedFriendIDs.length === 0) { statement = `你尚未選擇對象` }
 
         // NOT allowed if they are already in friendship 
         // between sender and some of the selected guys
-        if (rest[0].repetitionIDs.length !== 0){
+        if (rest[0].repetitionIDs.length !== 0) {
             let repeatIDString = "";
-            for ( let friend of rest[0].oldFriendsList) {
-                if (rest[0].repetitionIDs.includes(friend.user_id) ) {
+            for (let friend of rest[0].oldFriendsList) {
+                if (rest[0].repetitionIDs.includes(friend.user_id)) {
                     repeatIDString += ` ${friend.username} `;
-                }}
+                }
+            }
             statement = `你與${repeatIDString}已經是好友關係，請重新選擇對象`;
         }
 
@@ -103,51 +104,51 @@ export function ControlMsgBox(msgCssSelector, display, ...rest){
         return
     }
 
-    if (msgCssSelector === ".friend-response" && display === "block"){
+    if (msgCssSelector === ".friend-response" && display === "block") {
         // response
         let//
-        msgBox = document.querySelector(msgCssSelector),
-        msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
+            msgBox = document.querySelector(msgCssSelector),
+            msgBoxContent = document.querySelector(`${msgCssSelector} .content`);
 
-        if (window.sessionStorage.getItem("user_id")*1 === rest[0].senderID*1){
+        if (window.sessionStorage.getItem("user_id") * 1 === rest[0].senderID * 1) {
             msgBoxContent.textContent = (rest[0].accept) ? `${rest[0].receiverUsername} 接受你的好友邀請` : `${rest[0].rest[0].receiverUsername} 拒絕你的好友邀請`;
         }
 
-        if (window.sessionStorage.getItem("user_id")*1 === rest[0].receiverID*1){
+        if (window.sessionStorage.getItem("user_id") * 1 === rest[0].receiverID * 1) {
             msgBoxContent.textContent = (rest[0].accept) ? `你與 ${rest[0].senderUsername} 已結為好友` : `你已拒絕 ${rest[0].senderUsername} 的好友邀請`;
         }
-        
+
         msgBox.style.display = "block";
         return
     }
 }
 
-export function ClearList(cssSelector){
+export function ClearList(cssSelector) {
     let list = document.querySelector(cssSelector); //".main-pannel .create-list"
     while (list.hasChildNodes()) {
         list.removeChild(list.lastChild);
-    }    
+    }
 }
 
-export function RenderList(listCssSelector, listItemArray){
+export function RenderList(listCssSelector, listItemArray) {
     let//
-    teamListClass = [".create-list", "join-list"];
+        teamListClass = [".create-list", "join-list"];
 
     if (teamListClass.includes(listCssSelector)) {
         let list = document.querySelector(listCssSelector); //".create-list"
-        for ( let item of listItemArray) {
+        for (let item of listItemArray) {
             let team = document.createElement("div");
             team.setAttribute("class", "item");
             team.setAttribute("id", item.team_id);
             team.textContent = item.team_name;
             list.appendChild(team);
         }
-        return  
+        return
     }
 
-    if (listCssSelector === ".main-pannel .friends-list"){
+    if (listCssSelector === ".main-pannel .friends-list") {
         // Load friend list in main pannel
-        for ( let item of listItemArray) {
+        for (let item of listItemArray) {
             let friend = document.createElement("div");
             friend.setAttribute("class", "item");
             friend.setAttribute("id", item["user_id"]);
@@ -157,40 +158,40 @@ export function RenderList(listCssSelector, listItemArray){
         return
     }
 
-    if (listCssSelector === ".teams-pannel .friends-list"){
+    if (listCssSelector === ".teams-pannel .friends-list") {
         // Load friend list in team pannel
-        for ( let item of listItemArray) {
-        let//
-        friend = document.createElement("div"),
-        input = document.createElement("input"),
-        label = document.createElement("label");
+        for (let item of listItemArray) {
+            let//
+                friend = document.createElement("div"),
+                input = document.createElement("input"),
+                label = document.createElement("label");
 
-        friend.setAttribute("class", "item");
-        friend.setAttribute("id", item.user_id);
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id", item.user_id);
-        input.setAttribute("name", item.username);
-        label.setAttribute("for", item.username);
-        label.textContent = item.username;
+            friend.setAttribute("class", "item");
+            friend.setAttribute("id", item.user_id);
+            input.setAttribute("type", "checkbox");
+            input.setAttribute("id", item.user_id);
+            input.setAttribute("name", item.username);
+            label.setAttribute("for", item.username);
+            label.textContent = item.username;
 
-        friend.appendChild(input);
-        friend.appendChild(label);
-        friend.style.display = "none"; //switch to block when friend is online
-        DOMElements.teamPannelFriendsList.appendChild(friend);
+            friend.appendChild(input);
+            friend.appendChild(label);
+            friend.style.display = "none"; //switch to block when friend is online
+            DOMElements.teamPannelFriendsList.appendChild(friend);
         }
         return
     }
 }
 
-export function RenderOnlineStatus(itemCssSelector, onlineFriendArray){
+export function RenderOnlineStatus(itemCssSelector, onlineFriendArray) {
     let// 
         items = document.querySelectorAll(itemCssSelector), //".main-pannel .friends-list .item"
-        onlineStyle = {backgroundColor: "rgb(182, 232, 176)", border: "solid 3px rgb(22, 166, 6)"},
-        offlineStyle = {backgroundColor: "rgb(235, 234, 234)", border: "solid 3px rgb(182, 181, 181)"};
+        onlineStyle = { backgroundColor: "rgb(182, 232, 176)", border: "solid 3px rgb(22, 166, 6)" },
+        offlineStyle = { backgroundColor: "rgb(235, 234, 234)", border: "solid 3px rgb(182, 181, 181)" };
 
     if (itemCssSelector === ".main-pannel .friends-list .item") {
-        for ( let item of items ) {
-            if (onlineFriendArray.includes(item.getAttribute("id")*1)) {
+        for (let item of items) {
+            if (onlineFriendArray.includes(item.getAttribute("id") * 1)) {
                 item.style.backgroundColor = onlineStyle.backgroundColor;
                 item.style.border = onlineStyle.border;
                 continue;
@@ -203,8 +204,8 @@ export function RenderOnlineStatus(itemCssSelector, onlineFriendArray){
     }
 
     if (itemCssSelector === ".teams-pannel .friends-list .item") {
-        for ( let item of items ) {
-            if (onlineFriendArray.includes(item.getAttribute("id")*1)) {
+        for (let item of items) {
+            if (onlineFriendArray.includes(item.getAttribute("id") * 1)) {
                 item.style.display = "flex";
                 continue;
             }
