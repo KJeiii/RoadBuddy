8a02bc73876677488d0424439a30c6cdb5f9663a
KJeiii <pleasemailtokj@gmail.com>
2024-05-16 17:35:04 +0800

    move CheckUserStatus to ManageUser.js from CheckUseStatus.js and later one is deleted

--- RoadBuddy/views/static/Utils/ManageUser.js
+++ RoadBuddy/views/static/Utils/ManageUser.js
@@ -1,3 +1,34 @@
+// build function for checking user status
+export async function CheckUserStatus() {
+    let jwt = window.localStorage.getItem("token");
+
+    try{
+        if ( jwt === null) {return {"ok":false, "data": null}}
+
+        let//
+        response = await fetch("/api/member/auth", {
+            method: "GET",
+            headers: {"authorization": `Bearer ${jwt}`}
+        }),
+        result = await response.json();
+
+        if (result.data === null) {return {"ok": false, "data": null}}
+
+        let data = {
+            user_id: result.user_id,
+            username: result.username,
+            email: result.email
+        }
+    
+        return {"ok": true, "data": data}
+    }
+    catch(error) {
+        console.log(`Error in CheckUserStatus : ${error}`)
+        throw error
+    }
+}
+
+
 // cache user_id, username, team_id, email, friendList in sessionStorage
 export function ManipulateSessionStorage(storeOrRemoveOrClear, ...rest){
     try {
