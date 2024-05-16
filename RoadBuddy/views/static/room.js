13bbe1c390bd2031dd565a7349faf10c6aa7e2a9
KJeiii <pleasemailtokj@gmail.com>
2024-05-16 12:51:38 +0800

    move RenderOnlineStatus to friend.js; move codes about redirecting to member page owing to invalid jwt token to the top of first then statement, otherwise, any error could lead to redirection  when put rediction statement in the catch block

--- RoadBuddy/views/static/room.js
+++ RoadBuddy/views/static/room.js
@@ -4,73 +4,151 @@ import { SearchOldFriends } from "./Utils/ManageFriends.js";
 import { ClearList, RenderList } from "./Utils/GeneralControl.js";
 import { DrawMap, UserCoordError } from "./Utils/DrawMap.js";
 import * as AddEvents from "./Utils/AddEvents.js";
+import { ManipulateSessionStorage } from "./Utils/ManageUser.js";
 
-// check user status and load info when passing check
-CheckUserStatus()
-    .then((result) => {
-        let data = result.data;
-        // update main-pannel description
-        let description = document.querySelector(".main-pannel .description").textContent;
-        document.querySelector(".main-pannel .description").textContent = description + ` ${data.username}`;
+// ----- initialize socket.io -----
+socket.on("connect", ()=>{
+    // check user status and load info when passing check
+    CheckUserStatus()
+        .then((result) => {
+            if (!result.ok){window.location.replace("/member")};
 
-        // cache username, user_id, email in DOM sessionStorage
-        window.sessionStorage.setItem("username", data.username);
-        window.sessionStorage.setItem("user_id", data.user_id);
-        window.sessionStorage.setItem("email", data.email);
-        window.sessionStorage.removeItem("team_id");
-
-        // update friends list
-        SearchOldFriends(window.sessionStorage.getItem("user_id"))
+            let data = result.data;
+            // update main-pannel description
+            let description = document.querySelector(".main-pannel .description").textContent;
+            document.querySelector(".main-pannel .description").textContent = description + ` ${data.username}`;
+            
+            // store user info
+            sidArray.push(socket.id);
+            ManipulateSessionStorage("clear");
+            ManipulateSessionStorage("store", {...data, sid: socket.id})
+            
+            // render friend list
+            SearchOldFriends(data.user_id)
             .then((oldFriendList) => {
-                ClearList(".main-pannel .friends-list");
-                RenderList(".main-pannel .friends-list", oldFriendList);
+                    socket.emit("store_userinfo", {...data, friend_list: oldFriendList});
+                    socket.emit("initial_team_status");
+                    ManipulateSessionStorage("store", {friendList: JSON.stringify(oldFriendList)})
+                    // window.sessionStorage.setItem("friendList", JSON.stringify(oldFriendList))
+                    ClearList(".main-pannel .friends-list");
+                    RenderList(".main-pannel .friends-list", oldFriendList);
+                    
+                    ClearList(".teams-pannel .friends-list");
+                    RenderList(".teams-pannel .friends-list", oldFriendList);
+                })
+                .catch((error)=>{console.log(error)})
+            
 
-                ClearList(".teams-pannel .friends-list");
-                RenderList(".teams-pannel .friends-list", oldFriendList);
-            })
-            .catch((error)=>{console.log(error)})
+            // render team list
+            // 1. created team list
+            SearchTeams(data.user_id, "created")
+                .then((result) => {
+                    ClearList(".main-pannel .create-list");
+                    RenderList(".create-list", result.createdTeamList);
+                    AddEvents.AddEventsToTeamItems("created");
+                })
+                .catch((error) => console.log(
+                    `Error in render created team list (room.js):${error}`
+                ))
+                
+                // 2. joined team list 
+                SearchTeams(data.user_id, "joined")
+                .then((result) => {
+                    ClearList(".main-pannel .join-list");
+                    RenderList(".join-list", result.joinedTeamList);
+                    AddEvents.AddEventsToTeamItems("joined");
+                })
+                .catch((error) => console.log(
+                    `Error in render joined team list (room.js):${error}`
+                ))
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+
+    // test user's position by select position from randomPosition
+    DrawMap({coords: {
+        latitude: 24.982 + Math.random()*0.006,
+        longitude: 121.534 + Math.random()*0.006
+    }});
+
+
+})
+
+
+// // check user status and load info when passing check
+// CheckUserStatus()
+//     .then((result) => {
+//         let data = result.data;
+//         // update main-pannel description
+//         let description = document.querySelector(".main-pannel .description").textContent;
+//         document.querySelector(".main-pannel .description").textContent = description + ` ${data.username}`;
+
+//         // cache username, user_id, email in DOM sessionStorage
+//         window.sessionStorage.setItem("username", data.username);
+//         window.sessionStorage.setItem("user_id", data.user_id);
+//         window.sessionStorage.setItem("email", data.email);
+//         window.sessionStorage.removeItem("team_id");
+
+//         // update friends list
+//         SearchOldFriends(window.sessionStorage.getItem("user_id"))
+//             .then((oldFriendList) => {
+//                 window.sessionStorage.setItem("friendList", JSON.stringify(oldFriendList))
+//                 ClearList(".main-pannel .friends-list");
+//                 RenderList(".main-pannel .friends-list", oldFriendList);
+
+//                 ClearList(".teams-pannel .friends-list");
+//                 RenderList(".teams-pannel .friends-list", oldFriendList);
+//             })
+//             .then(()=>{
+//                 console.log(onlineFriendArray)
+//                 RenderOnlineStatus(".main-pannel .friends-list .item", onlineFriendArray);
+//                 RenderOnlineStatus(".teams-pannel .friends-list .item", onlineFriendArray);
+//             })
+//             .catch((error)=>{console.log(error)})
          
 
-        // render team list
-        // 1. created team list
-        SearchTeams(data.user_id, "created")
-            .then((result) => {
-                ClearList(".main-pannel .create-list");
-                RenderList(".create-list", result.createdTeamList);
-                AddEvents.AddEventsToTeamItems("created");
-            })
-            .catch((error) => console.log(
-                `Error in render created team list (room.js):${error}`
-            ))
+//         // render team list
+//         // 1. created team list
+//         SearchTeams(data.user_id, "created")
+//             .then((result) => {
+//                 ClearList(".main-pannel .create-list");
+//                 RenderList(".create-list", result.createdTeamList);
+//                 AddEvents.AddEventsToTeamItems("created");
+//             })
+//             .catch((error) => console.log(
+//                 `Error in render created team list (room.js):${error}`
+//             ))
 
-        // 2. joined team list 
-        SearchTeams(data.user_id, "joined")
-            .then((result) => {
-                ClearList(".main-pannel .join-list");
-                RenderList(".join-list", result.joinedTeamList);
-                AddEvents.AddEventsToTeamItems("joined");
-            })
-            .catch((error) => console.log(
-                `Error in render joined team list (room.js):${error}`
-            ))        
-    })
-    .catch((error) => {
-        console.log(error);
-        window.location.replace("/member");
-    })
+//         // 2. joined team list 
+//         SearchTeams(data.user_id, "joined")
+//             .then((result) => {
+//                 ClearList(".main-pannel .join-list");
+//                 RenderList(".join-list", result.joinedTeamList);
+//                 AddEvents.AddEventsToTeamItems("joined");
+//             })
+//             .catch((error) => console.log(
+//                 `Error in render joined team list (room.js):${error}`
+//             ))        
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//         window.location.replace("/member");
+//     })
 
 
-// test user's position by select position from randomPosition
-let testCoords = {
-    latitude: 24.982 + Math.random()*0.006,
-    longitude: 121.534 + Math.random()*0.006
-};
-DrawMap({
-    coords: {
-        latitude: testCoords.latitude,
-        longitude: testCoords.longitude
-    }
-});
+// // test user's position by select position from randomPosition
+// let testCoords = {
+//     latitude: 24.982 + Math.random()*0.006,
+//     longitude: 121.534 + Math.random()*0.006
+// };
+// DrawMap({
+//     coords: {
+//         latitude: testCoords.latitude,
+//         longitude: testCoords.longitude
+//     }
+// });
 
 
 // ----- draw initial map -----
