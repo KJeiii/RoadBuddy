fc6141c073046646c33d50d1320cd566394493de
KJeiii <pleasemailtokj@gmail.com>
2024-06-08 16:07:25 +0800

    register message blueprint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from RoadBuddy.views.routes import *
 from RoadBuddy.controllers.member import member_bp
 from RoadBuddy.controllers.friend import friend_bp
 from RoadBuddy.controllers.team import team_bp
+from RoadBuddy.controllers.message import message_bp
 from RoadBuddy.event_handler.connect import *
 from RoadBuddy.event_handler.friend import *
 from RoadBuddy.event_handler.team import *
@@ -12,6 +13,6 @@ from RoadBuddy.event_handler.tracking import *
 app.register_blueprint(member_bp)
 app.register_blueprint(friend_bp)
 app.register_blueprint(team_bp)
-
+app.register_blueprint(message_bp)
 
 socketio.run(app, debug=True, port=3000, allow_unsafe_werkzeug=True)
