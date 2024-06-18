4b447c9646d384f19bf834f5df3a71abefc35ca7
KJeiii <pleasemailtokj@gmail.com>
2024-06-18 12:16:00 +0800

    add one more condition: if receiver is traveling, do not show request prompt; instead, store in message db

--- RoadBuddy/event_handler/friend.py
+++ RoadBuddy/event_handler/friend.py
@@ -13,7 +13,8 @@ def friend_request(data):
 
     for id in data["receiverIDs"]:
         is_receiver_online = user_info.get(id) != None
-        if is_receiver_online:
+        is_receiver_in_traveling = user_info.get(id).get("team_id") != None
+        if is_receiver_online and not is_receiver_in_traveling:
             sender_data = {
                 "sid": sender_sid,
                 "user_id": sender_id,
@@ -22,9 +23,11 @@ def friend_request(data):
             }
             emit("friend_request", sender_data, to=user_info[id]["sid"])
             print(f'{user_info[sender_id]["username"]} sends request to {user_info[id]["username"]}')
+        elif is_receiver_online and is_receiver_in_traveling:
+            messageTool.Create_message(id, sender_id)
+            emit("update_message", messageTool.Search_message(id), to=user_info.get(id).get("sid"))
         else:
             messageTool.Create_message(id, sender_id)
-
 
 
 @socketio.on("friend_request_result")
