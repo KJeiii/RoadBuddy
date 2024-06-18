810f2dce02fc665896d948a6a70a7236eec7ad1d
KJeiii <pleasemailtokj@gmail.com>
2024-06-18 12:18:00 +0800

    create socket event handler for update_message

--- RoadBuddy/views/static/event_handler/message.js
+++ RoadBuddy/views/static/event_handler/message.js
@@ -1,97 +1,105 @@
-let alertBtn = document.querySelector(".alert button");
-alertBtn.addEventListener("click", () => {
-    let data = {
-        sender_sid: window.sessionStorage.getItem("sid"),
-        receiver_sid: document.querySelector(".alert input[name=sid]").value,
-        msg: document.querySelector(".alert input[name=msg]").value
-    };
+import { messageInfo } from "../main.js";
 
-    socket.emit("alert", data);
+socket.on("update_message", (messages)=>{
+    messages.forEach((message) => {
+        messageInfo.UpdateInfo(message)})
+    messageInfo.ShowInfo();
 })
 
-socket.on("alert", (data) => {
-    let//
-    prompt = document.querySelector(".prompt"),
-    content = document.querySelector(".prompt .content");
+// let alertBtn = document.querySelector(".alert button");
+// alertBtn.addEventListener("click", () => {
+//     let data = {
+//         sender_sid: window.sessionStorage.getItem("sid"),
+//         receiver_sid: document.querySelector(".alert input[name=sid]").value,
+//         msg: document.querySelector(".alert input[name=msg]").value
+//     };
 
-    content.textContent = `Do you want to receive msg from ${data.sender_sid}`;
-    prompt.style.display = "block";
+//     socket.emit("alert", data);
+// })
 
-    let//
-    yesBtn = document.querySelector(".yes"),
-    noBtn = document.querySelector(".no");
+// socket.on("alert", (data) => {
+//     let//
+//     prompt = document.querySelector(".prompt"),
+//     content = document.querySelector(".prompt .content");
 
-    noBtn.addEventListener("click", () => {
-        content.textContent = "";
-        prompt.style.display = "none";
-    })
+//     content.textContent = `Do you want to receive msg from ${data.sender_sid}`;
+//     prompt.style.display = "block";
 
-    yesBtn.addEventListener("click", () => {
-        let//
-        alertDiv = document.querySelector(".alert"),
-        msgDiv = document.createElement("div");
+//     let//
+//     yesBtn = document.querySelector(".yes"),
+//     noBtn = document.querySelector(".no");
 
-        msgDiv.textContent = data.msg;
-        alertDiv.appendChild(msgDiv);
+//     noBtn.addEventListener("click", () => {
+//         content.textContent = "";
+//         prompt.style.display = "none";
+//     })
 
-        content.textContent = "";
-        prompt.style.display = "none";
-    })
-})
+//     yesBtn.addEventListener("click", () => {
+//         let//
+//         alertDiv = document.querySelector(".alert"),
+//         msgDiv = document.createElement("div");
 
+//         msgDiv.textContent = data.msg;
+//         alertDiv.appendChild(msgDiv);
 
-// -------- msg test for room -------
-// test leave team for alert message
-let msgleaveTeamBtn = document.querySelector("div.alert button.leave");
-msgleaveTeamBtn.addEventListener("click", ()=> {
-    // switch to main pannel
-    friendsPannel.style.display = "none";
-    teamsPannel.style.display = "none";
-    trackingPannel.style.display = "none";
-    mainPannel.style.display = "none";
-
-    // change elements in setting div
-    config.style.display = "none";
-    logout.style.display = "none";
-    leave.style.display = "none";
-
-    // organize data for emitted to event listener "leave_team" on server
-    let data = {
-        team_id: `${window.sessionStorage.getItem("team_id")}`,
-        username: window.sessionStorage.getItem("username"),
-        user_id: window.sessionStorage.getItem("user_id"),
-        email: window.sessionStorage.getItem("email")
-    };
-    socket.emit("leave_team", data);
-    window.sessionStorage.removeItem("team_id");
-})
+//         content.textContent = "";
+//         prompt.style.display = "none";
+//     })
+// })
 
 
-// test alert msg emitted to room
-let btnAlert = document.querySelector("button.alert");
-alertBtn.addEventListener("click", ()=>{
-    let//
-    msg = document.querySelector("div.alert input").value,
-    data = {
-        team_id: `${window.sessionStorage.getItem("team_id")}`,
-        msg: msg,
-        user_id: window.sessionStorage.getItem('user_id'),
-        username: window.sessionStorage.getItem('username'),
-        email: window.sessionStorage.getItem('email'),
-    };
+// // -------- msg test for room -------
+// // test leave team for alert message
+// let msgleaveTeamBtn = document.querySelector("div.alert button.leave");
+// msgleaveTeamBtn.addEventListener("click", ()=> {
+//     // switch to main pannel
+//     friendsPannel.style.display = "none";
+//     teamsPannel.style.display = "none";
+//     trackingPannel.style.display = "none";
+//     mainPannel.style.display = "none";
 
-    socket.emit("alert", data);
-    console.log(`${window.sessionStorage.getItem("username")} send ${msg}`);
-})
+//     // change elements in setting div
+//     config.style.display = "none";
+//     logout.style.display = "none";
+//     leave.style.display = "none";
 
-socket.on("alert", (data)=>{
-    console.log(`Receive ${data.msg} from ${data.username}`);
+//     // organize data for emitted to event listener "leave_team" on server
+//     let data = {
+//         team_id: `${window.sessionStorage.getItem("team_id")}`,
+//         username: window.sessionStorage.getItem("username"),
+//         user_id: window.sessionStorage.getItem("user_id"),
+//         email: window.sessionStorage.getItem("email")
+//     };
+//     socket.emit("leave_team", data);
+//     window.sessionStorage.removeItem("team_id");
+// })
 
-    let//
-    msg = data.msg,
-    alert = document.querySelector("div.alert"),
-    msgDiv = document.createElement("div");
 
-    msgDiv.textContent = msg;
-    alert.appendChild(msgDiv);
-})
+// // test alert msg emitted to room
+// let btnAlert = document.querySelector("button.alert");
+// alertBtn.addEventListener("click", ()=>{
+//     let//
+//     msg = document.querySelector("div.alert input").value,
+//     data = {
+//         team_id: `${window.sessionStorage.getItem("team_id")}`,
+//         msg: msg,
+//         user_id: window.sessionStorage.getItem('user_id'),
+//         username: window.sessionStorage.getItem('username'),
+//         email: window.sessionStorage.getItem('email'),
+//     };
+
+//     socket.emit("alert", data);
+//     console.log(`${window.sessionStorage.getItem("username")} send ${msg}`);
+// })
+
+// socket.on("alert", (data)=>{
+//     console.log(`Receive ${data.msg} from ${data.username}`);
+
+//     let//
+//     msg = data.msg,
+//     alert = document.querySelector("div.alert"),
+//     msgDiv = document.createElement("div");
+
+//     msgDiv.textContent = msg;
+//     alert.appendChild(msgDiv);
+// })
