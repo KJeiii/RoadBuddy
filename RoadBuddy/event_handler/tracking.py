1163ee9dab2733d2ac84ff6aef9d2edcfccf5b03
KJeiii <pleasemailtokj@gmail.com>
2024-06-29 12:28:36 +0800

    modify import syntax from 'from Roadbuddy.event_handler import variable to import Roadbuddy.event_handler'

--- RoadBuddy/event_handler/tracking.py
+++ RoadBuddy/event_handler/tracking.py
@@ -1,7 +1,7 @@
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from RoadBuddy import socketio
 from flask import request, session
-from RoadBuddy.event_handler import rooms_info, own_coords, user_info, sid_reference
+import RoadBuddy.event_handler
 
 
 
@@ -13,37 +13,37 @@ def position(data):
 
     # If not in team, update position to own map
     if team_id == None:
-        if len(own_coords) >= 2:
-            del own_coords[0]
-            own_coords.append(new_coord)
-            data = {request.sid : own_coords}
+        if len(RoadBuddy.event_handler.own_coords) >= 2:
+            del RoadBuddy.event_handler.own_coords[0]
+            RoadBuddy.event_handler.own_coords.append(new_coord)
+            data = {request.sid : RoadBuddy.event_handler.own_coords}
             emit("movingPostion", data, to=request.sid)
 
-        if len(own_coords) == 1 :
-            own_coords.append(new_coord)
-            data = {request.sid : own_coords}
+        if len(RoadBuddy.event_handler.own_coords) == 1 :
+            RoadBuddy.event_handler.own_coords.append(new_coord)
+            data = {request.sid : RoadBuddy.event_handler.own_coords}
             # emit("initPosition", data, to=request.sid)
 
-        if len(own_coords) == 0 :
-            own_coords.append(new_coord)
-            data = {request.sid : own_coords}
+        if len(RoadBuddy.event_handler.own_coords) == 0 :
+            RoadBuddy.event_handler.own_coords.append(new_coord)
+            data = {request.sid : RoadBuddy.event_handler.own_coords}
             # emit("initPosition", data, to=request.sid)
         return
 
     # If in a team, update all partners position
-    user_coords = rooms_info.get(team_id)["partner"].get(sid)
+    user_coords = RoadBuddy.event_handler.rooms_info.get(team_id)["partner"].get(sid)
 
     if len(user_coords) >= 2:
-        del rooms_info[team_id]["partner"][sid][0]
-        rooms_info[team_id]["partner"][sid].append(new_coord)
-        emit("movingPostion", rooms_info[team_id]["partner"], to=team_id)
+        del RoadBuddy.event_handler.rooms_info[team_id]["partner"][sid][0]
+        RoadBuddy.event_handler.rooms_info[team_id]["partner"][sid].append(new_coord)
+        emit("movingPostion", RoadBuddy.event_handler.rooms_info[team_id]["partner"], to=team_id)
 
     if len(user_coords) == 1 :
-        rooms_info[team_id]["partner"][sid].append(new_coord)
-        emit("initPosition", rooms_info[team_id]["partner"], to=team_id)
+        RoadBuddy.event_handler.rooms_info[team_id]["partner"][sid].append(new_coord)
+        emit("initPosition", RoadBuddy.event_handler.rooms_info[team_id]["partner"], to=team_id)
 
     if len(user_coords) == 0 :
-        rooms_info[team_id]["partner"][sid].append(new_coord)
-        # emit("initPosition", rooms_info[team_id], to=team_id)
+        RoadBuddy.event_handler.rooms_info[team_id]["partner"][sid].append(new_coord)
+        # emit("initPosition", RoadBuddy.event_handler.rooms_info[team_id], to=team_id)
     
 
