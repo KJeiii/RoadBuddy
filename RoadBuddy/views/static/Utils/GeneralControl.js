fc49c491acd07e9ac012f818494d487044a966ce
KJeiii <pleasemailtokj@gmail.com>
2024-07-17 15:23:58 +0800

    add config button in btnsForMainPannel when trigger SwitchSettingBtn function

--- RoadBuddy/views/static/Utils/GeneralControl.js
+++ RoadBuddy/views/static/Utils/GeneralControl.js
@@ -162,7 +162,7 @@ export function SwitchSettingBtn(...manualSwitch){
     }
 
     const//
-        btnsForMainPannel = [DOMElements.logout, DOMElements.message], //DOMElements.config
+        btnsForMainPannel = [DOMElements.logout, DOMElements.message, DOMElements.config], 
         btnsForTrackingPannel = [DOMElements.invite, DOMElements.leave];
     
     const onMainPannel = (DOMElements.mainPannel.style.display === "block");
