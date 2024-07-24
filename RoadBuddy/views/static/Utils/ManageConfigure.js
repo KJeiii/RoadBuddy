e307e61664430a7c1b983da58041628faa009c80
KJeiii <pleasemailtokj@gmail.com>
2024-07-24 12:39:48 +0800

    modify CollectUpdateBasicInfo function to be async function to check if any new information needs to be updated; add responseCode in data return by UpdateBasicInfo; create RenderUpdateResponse function

--- RoadBuddy/views/static/Utils/ManageConfigure.js
+++ RoadBuddy/views/static/Utils/ManageConfigure.js
@@ -1,4 +1,3 @@
-
 export function PreviewAvatar(){
     const//
         avatarImg = document.querySelector("input#avatar").files[0],
@@ -11,36 +10,54 @@ export function PreviewAvatar(){
     fileReader.readAsDataURL(avatarImg)
 }
 
-export function CollectUpdateBasicInfo(){
+export async function CollectUpdateBasicInfo(){
     const//
-        dataToUpdate = new FormData(),
-        avatarFile = document.querySelector("input#avatar").files[0],
-        usernameToUpdate = document.querySelector("input#username-to-update").value;
-    dataToUpdate.append("usernameToUpdate", usernameToUpdate);
-    dataToUpdate.append("avatar", avatarFile);
-    return dataToUpdate
+        hasFile = document.querySelector("input#avatar").files.length > 0,
+        hasNewUsername = document.querySelector("input#username-to-update").value 
+                        !== window.sessionStorage.getItem("username");
+
+    if (hasFile || hasNewUsername){
+        const//
+            avatarFile = document.querySelector("input#avatar").files[0],
+            usernameToUpdate = document.querySelector("input#username-to-update").value,
+            dataToUpdate = new FormData();
+        dataToUpdate.append("usernameToUpdate", usernameToUpdate);
+        dataToUpdate.append("avatar", avatarFile);
+        return dataToUpdate
+    }
+    throw "There is no new information to update."
 }
 
 export async function UpdateBasicInfo(formDataToUpdate){
     try{
         const response = await fetch("/api/member/update/basic", {
             method: "PATCH",
+            headers: {"authorization": `Bearer ${window.localStorage.getItem("token")}`},
             body: formDataToUpdate
         });
-        if (!response.ok){throw {ok: false, message: response.statusText}}
+        if (!response.ok){throw {responseCode: 0, message: response.statusText}}
         const result = await response.json();
-        return {ok: true, message: result}
+        return {...result, responseCode: 1}
     }
     catch(error){
         console.log("Failed to execute UpdateBasicInfo: ", error)
-        throw {ok: false, message: error}
+        throw {responseCode: 0, message: error}
     }
 }
 
-export function RenderUpdateResult(isSuccess){
+export function RenderUpdateResponse(responseCode){
+    document.querySelector(".configure-pannel").style.display = "none";
+    document.querySelector(".configure-response").style.display = "flex";
     const//
         responseTitle = document.querySelector(".configure-response p"),
         img = document.querySelector(".configure-response img");
-    responseTitle.textContent = isSuccess ? "更新完成" : "更新失敗";
-    img.src =  isSuccess ? "../static/images/check.gif" : "../static/images/error.gif";
+
+    const resultObj = {
+        0: {title: "更新失敗", img_src: "../static/images/error.gif"},
+        1: {title: "更新完成", img_src: "../static/images/check.gif"},
+        2: {title: "無資料需更新", img_src: "../static/images/error.gif"},
+        3: {title: "更新中", img_src: "../static/images/loading.gif"}
+    };
+    responseTitle.textContent = resultObj[responseCode].title;
+    img.src = resultObj[responseCode].img_src;
 }
