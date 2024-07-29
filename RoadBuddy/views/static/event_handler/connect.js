371268b968b9f123533a38987b8fbce33f534797
KJeiii <pleasemailtokj@gmail.com>
2024-07-29 16:43:12 +0800

    move leaving team related code to socket event of leave_team in team.js

--- RoadBuddy/views/static/event_handler/connect.js
+++ RoadBuddy/views/static/event_handler/connect.js
@@ -5,23 +5,6 @@ socket.on("initialization", () => {
     socket.emit("online_friend_status");
 });
 
-//  Listener for receiving event "disconnect" event from server  
-socket.on("disconnect", (data) => {
-    let sid = data.sid;
-
-    // remove marker when user disconnects
-    map.removeLayer(markerArray[sidArray.indexOf(sid)]);
-
-    // clear user cache
-    markerArray.splice(sidArray.indexOf(sid),1);
-    sidArray.splice(sidArray.indexOf(sid),1);
-    
-    // clear team_id when partner disconnects
-    if (data["user_id"]*1 === window.sessionStorage.getItem("user_id")*1) {
-        window.sessionStorage.removeItem("team_id");
-    }
-});
-
 socket.on("sync_online_user", (userIDArray) => {
     onlineUserInfo.AppendUserID(userIDArray)
 })
