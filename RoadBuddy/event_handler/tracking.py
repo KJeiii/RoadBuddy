d355b7c03097ac7948a716b1033713e190d22820
KJeiii <pleasemailtokj@gmail.com>
2024-07-29 16:24:13 +0800

    rename data received

--- RoadBuddy/event_handler/tracking.py
+++ RoadBuddy/event_handler/tracking.py
@@ -4,11 +4,11 @@ from flask import request, session
 import RoadBuddy.event_handler
 
 @socketio.on("position")
-def position(data):
-    sid = data.get("sid")
-    new_coordination = data.get("coordination")
-    team_id = data.get("team_id")
-    is_user_in_team = RoadBuddy.event_handler.rooms_info[team_id]["partners"].get(sid) != None
+def position(new_movement):
+    sid = new_movement.get("sid")
+    new_coordination = new_movement.get("coordination")
+    team_id = new_movement.get("team_id")
+    is_user_in_team = sid in RoadBuddy.event_handler.rooms_info[team_id]["partners"].keys()
 
     if is_user_in_team:
         RoadBuddy.event_handler.rooms_info[team_id]["partners"][sid]["coordination"] = new_coordination
