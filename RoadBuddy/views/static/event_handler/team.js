180199fe66aff57bd3627c88c2810fd54a76e021
KJeiii <pleasemailtokj@gmail.com>
2024-07-29 16:45:19 +0800

    do not remove own marker in the socket event leave_team; fix fail to send join team request and accept application

--- RoadBuddy/views/static/event_handler/team.js
+++ RoadBuddy/views/static/event_handler/team.js
@@ -9,7 +9,6 @@ import { mapInfo } from "../main.js";
 // ----- listener for receiving event "team_invite" from server -----
 socket.on("team_invite", (data) => {
     team_sender_info_cache = data
-
     // prompt to ask willness
     ControlTeamMsgBox(".team-invite-prompt", "block", {leaderName: data.username})
 })
@@ -47,10 +46,13 @@ socket.on("add_partner", (partner) => {
 })
 
 // ----- Listener for receiving event "leave_team" from server -----
-socket.on("leave_team", (data) => {
+socket.on("leave_team", (leavingUser) => {
     // remove leaving partner's marker and information in the partner list
-    mapInfo.RemoveMarker(data.sid);
-    RemoveUserFromPartnerList(data["user_id"])
+    const isNotMe = leavingUser.sid !== socket.id;
+    if (isNotMe){
+        mapInfo.RemoveMarker(leavingUser.sid);
+        RemoveUserFromPartnerList(leavingUser["user_id"])
+    }
 })
 
 socket.on("update_team_status", (teamArray) => {
@@ -64,41 +66,40 @@ socket.on("update_team_status", (teamArray) => {
 })
 
 // (Team owner) Listener for receiving event "join_team_reqeust" from server
-socket.on("join_team_request", (data) => {
+socket.on("join_team_request", (applicant) => {
     // show prompt of the request for joining team
     ControlTeamMsgBox(".team-join-request", "block", {
-        requesterID: data["user_id"],
-        requesterSID: data["user_sid"],
-        requesterName:data["username"]
+        applicantID: applicant.userID,
+        applicantSID: applicant.userSID,
+        applicantUsername: applicant.username
     });
+    team_applicants_cache[applicant.userID] = {...applicant};
 })
 
 // (Requester) Listener for receiving event "accept_team_request" from team owner
-socket.on("accept_team_request", (data) => {
+socket.on("accept_team_request", (acceptApplicationResponse) => {
     // cache sender_info
-    team_sender_info_cache = data;
+    team_sender_info_cache = acceptApplicationResponse;   //<<<<<<<<<<<<<<<<<<
+    const {team_id:teamID, partners} = acceptApplicationResponse;
 
     // Organize data emitted to listener "enter_team" on server
-    EmitEnterTeamEvent(true, "join", data["team_id"]);
-    ManipulateSessionStorage("set", {team_id: data["team_id"]});
+    EmitEnterTeamEvent(true, "join", teamID, myCoord);
+    ManipulateSessionStorage("set", {team_id: teamID});
 
     // switch to tracking pannel
     SwitchPannel("tracking");
 
-    // create partner information in partner-list
-    // 1. only show team owner and partner it self
-    // 2. update other partners when they join in
-    let partnerList = document.querySelector(".tracking-pannel .partner-list");
-    for ( let id in team_sender_info_cache["partners_color"] ) {
-
-        // create leader item first
-        if ( id*1 === team_sender_info_cache["user_id"]*1 ) {
-            AppendUserInPartnerList(id, partnerList, team_sender_info_cache["partners_color"]);
-        }
-
-        // then, create partner self 
-        if ( id*1 === window.sessionStorage.getItem("user_id")*1 ) {
-            AppendUserInPartnerList(id, partnerList, team_sender_info_cache["partners_color"]);
+    // append partners icon and markers
+    const//
+        {user_id:userID, username, image_url:imageUrl} = window.sessionStorage,
+        partnerList = document.querySelector(".tracking-pannel .partner-list");
+    AppendUserInPartnerList(userID, username, imageUrl, partnerList);
+    for (const partner in partners){
+        const isNotMe = partner !== socket.id;
+        if (isNotMe){
+            console.log(partner);
+            AppendUserInPartnerList(partners[partner].user_id, partners[partner].username, partners[partner].image_url, partnerList);
+            mapInfo.CreateMarker(partner, partners[partner].image_url, partners[partner].coordination);
         }
     }
 })
