977f3773c55ba6316c084d42b4a36a065cc63d46
KJeiii <pleasemailtokj@gmail.com>
2024-07-31 21:41:16 +0800

    use CreatePartner function to wrap up CreateMarker and CreateIconImage

--- RoadBuddy/views/static/event_handler/team.js
+++ RoadBuddy/views/static/event_handler/team.js
@@ -1,8 +1,8 @@
 import { ClearList, RenderList, RenderOnlineStatus, SwitchPannel, ControlTeamMsgBox
 } from "../Utils/GeneralControl.js";
-import { AppendUserInPartnerList, RemoveUserFromPartnerList } from "../Utils/ManagePartner.js";
+import { AppendUserInPartnerList, CreatePartner, RemoveUserFromPartnerList } from "../Utils/ManagePartner.js";
 import { AddTeamClickEvent, AddTeamHoverEvent } from "../Utils/TeamEvent.js";
-import { ManipulateSessionStorage } from "../Utils/ManageUser.js";
+import { ClearCanvasContext, CreateIconImage, ManipulateSessionStorage } from "../Utils/ManageUser.js";
 import { EmitEnterTeamEvent } from "../Utils/ManageTeam.js";
 import { mapInfo } from "../main.js";
 
@@ -16,11 +16,11 @@ socket.on("team_invite", (data) => {
 //  ----- add new partners if they join -----
 socket.on("add_partner", (partner) => { 
     const//
-        {user_id:userID, sid, username, image_url:imageUrl, coordination} = partner,
+        {user_id:userID, sid, username, image_url:imageUrl, coordination, icon_color: iconColor} = partner,
         notMyself = userID !== Number(window.sessionStorage.getItem("user_id"));
     if (notMyself){
-        mapInfo.CreateMarker(sid, imageUrl, coordination);
-        AppendUserInPartnerList(userID, username, imageUrl, document.querySelector(".tracking-pannel .partner-list"));
+        CreatePartner(userID, sid, username, imageUrl, iconColor, coordination, 
+            document.querySelector(".tracking-pannel .partner-list"));
     }
 })
 
@@ -57,12 +57,12 @@ socket.on("join_team_request", (applicant) => {
 
 // (Requester) Listener for receiving event "accept_team_request" from team owner
 socket.on("accept_team_request", (acceptApplicationResponse) => {
-    // cache sender_info
-    team_sender_info_cache = acceptApplicationResponse;   
+    // // cache sender_info
+    // team_sender_info_cache = acceptApplicationResponse;   
     const {team_id:teamID, partners} = acceptApplicationResponse;
 
     // Organize data emitted to listener "enter_team" on server
-    EmitEnterTeamEvent(true, "join", teamID, myCoord);
+    EmitEnterTeamEvent(true, "join", teamID, window.sessionStorage.getItem("iconColor"), myCoord);
     ManipulateSessionStorage("set", {team_id: teamID});
 
     // switch to tracking pannel
@@ -76,9 +76,8 @@ socket.on("accept_team_request", (acceptApplicationResponse) => {
     for (const partner in partners){
         const isNotMe = partner !== socket.id;
         if (isNotMe){
-            console.log(partner);
-            AppendUserInPartnerList(partners[partner].user_id, partners[partner].username, partners[partner].image_url, partnerList);
-            mapInfo.CreateMarker(partner, partners[partner].image_url, partners[partner].coordination);
+            const {user_id:userID, username, image_url:imageUrl, coordination, icon_color:iconColor} = partners[partner];
+            CreatePartner(userID, partner, username, imageUrl, iconColor, coordination, partnerList);
         }
     }
 })
