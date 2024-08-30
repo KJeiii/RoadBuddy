f05d69a333247697f65f73671e2642e5f87970d3
KJeiii <pleasemailtokj@gmail.com>
2024-08-30 10:50:20 +0800

    add AWS RWD dbconfigure

--- RoadBuddy/models/__init__.py
+++ RoadBuddy/models/__init__.py
@@ -2,9 +2,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+# local for development
 db_config = {
     "host": "localhost",
     "user": "root",
     "password": os.environ.get("dbpassword"),
-    "database": "roadbuddy"
+    "database": "roadbuddy",
 }
+
+# AWS RDS
+# db_config = {
+#     "host": os.environ.get("db_host"),
+#     "port": os.environ.get("db_port"),
+#     "user": os.environ.get("db_user"),
+#     "password": os.environ.get("db_password"),
+#     "database": os.environ.get("database")
+# }
