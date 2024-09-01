ef78ce6a1809385511519fe7bfd553353c859789
KJeiii <pleasemailtokj@gmail.com>
2024-09-01 12:52:32 +0800

    rename RenderUpdateResponse with RendeResponse and move to GeneralControl.js

--- RoadBuddy/views/static/Utils/ManageConfigure.js
+++ RoadBuddy/views/static/Utils/ManageConfigure.js
@@ -13,24 +13,6 @@ export function SwitchAvatarUndoBtn(undoBtnCssSelector){
     undoBtn.style.display = (isShown) ? "none" : "block";
 }
 
-export function RenderUpdateResponse(responseCode){
-    document.querySelector(".configure-pannel").style.display = "none";
-    document.querySelector(".configure-response").style.display = "flex";
-    const//
-        responseTitle = document.querySelector(".configure-response p"),
-        img = document.querySelector(".configure-response img");
-
-    const resultObj = {
-        0: {title: "更新失敗", img_src: "../static/images/error.gif"},
-        1: {title: "更新完成", img_src: "../static/images/check.gif"},
-        2: {title: "更新中", img_src: "../static/images/loading.gif"},
-        3: {title: "無資料需更新", img_src: "../static/images/error.gif"},
-        4: {title: "舊密碼不正確", img_src: "../static/images/error.gif"}
-    };
-    responseTitle.textContent = resultObj[responseCode].title;
-    img.src = resultObj[responseCode].img_src;
-}
-
 export function SwitchChangePasswordPrompt(){
     const//
         prompt = document.querySelector("div.update-password"),
