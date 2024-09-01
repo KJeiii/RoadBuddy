4275e56ba34e47c3642828e13cc4fdf823495ba0
KJeiii <pleasemailtokj@gmail.com>
2024-09-01 12:58:36 +0800

    modify RenderResponse-related code and wrap up loggin-related code with function Login()

--- RoadBuddy/views/static/member.js
+++ RoadBuddy/views/static/member.js
@@ -1,8 +1,8 @@
-import { ClearErrorMessage, ControlMebmerMsgBox, RenderErrorMessage, SwitchBetweenSignupAndLogin, 
+import { ClearErrorMessage, ControlMebmerMsgBox, RenderErrorMessage, RenderResponse, SwitchBetweenSignupAndLogin, 
     SwitchSignUpStep, VerifyInputValue, isEmailInputPass, isInputFilledIn, isInputValueIncludingCharacters, 
     isInputValuesConsistent, isPasswordInputPass, isUsernameInputPass} from "./Utils/GeneralControl.js";
 import { PreviewAvatar, SwitchAvatarUndoBtn } from "./Utils/ManageConfigure.js";
-import { CheckUserStatus, CollectInformationToSignup, SignupNewAccount } from "./Utils/ManageUser.js";
+import { CheckUserStatus, CollectInformationToSignup, Login, SignupNewAccount } from "./Utils/ManageUser.js";
 
 CheckUserStatus()
     .then((result) => {
@@ -84,14 +84,18 @@ document.querySelector("button.signup").addEventListener("click", () => {
         ControlMebmerMsgBox("div.signup-prompt", "flex");
         return
     }
+    RenderResponse(".member-response", 1);
     SignupNewAccount(CollectInformationToSignup())
         .then((signupResponse) => {
             if (!signupResponse.ok){
                 SwitchSignUpStep();
                 RenderErrorMessage(document.querySelector("div.signup div.form-div-title.email"), signupResponse.message);
+                RenderResponse(".member-response", 0, true);    
                 return
-            }
+            };
+            RenderResponse(".member-response", 2);
             SwitchBetweenSignupAndLogin();
+            RenderResponse(".member-response", 0, true);
             document.querySelector("div.login input[name=email]").value = signupResponse.email;
         })
         .catch((error)=>{console.log(error)})
@@ -104,15 +108,19 @@ document.querySelector("div.signup-prompt button.no").addEventListener("click", 
 
 // --- click yes button of signup prompt ---
 document.querySelector("div.signup-prompt button.yes").addEventListener("click", ()=>{
+    ControlMebmerMsgBox("div.signup-prompt", "none");
+    RenderResponse(".member-response", 1);
     SignupNewAccount(CollectInformationToSignup())
         .then((signupResponse) => {
             if (!signupResponse.ok){
                 SwitchSignUpStep();
+                RenderResponse(".member-response", 0, true);
                 RenderErrorMessage(document.querySelector("div.signup div.form-div-title.email"), signupResponse.message);
                 return
             }
-            ControlMebmerMsgBox("div.signup-prompt", "none");
+            RenderResponse(".member-response", 2);
             SwitchBetweenSignupAndLogin();
+            RenderResponse(".member-response", 0, true);
             document.querySelector("div.login input[name=email]").value = signupResponse.email;
         })
         .catch((error)=>{console.log(error)})
@@ -120,22 +128,14 @@ document.querySelector("div.signup-prompt button.yes").addEventListener("click",
 
 // --- login ---
 document.querySelector("button.login").addEventListener("click", async() => {
-    let//
-    emailInput = document.querySelector("div.login input[name=email]"),
-    passwordInput = document.querySelector("div.login input[name=password]");
+    RenderResponse(".member-response", 3);
+    ClearErrorMessage(
+        document.querySelector("div.login div.form-div-title.email"),
+        document.querySelector("div.login div.form-div-title.password")
+    );
 
-    let emailTitle = document.querySelector("div.login div.form-div-title.email");
-    while ( emailTitle.childNodes.length > 2 ) {
-        emailTitle.removeChild(emailTitle.lastChild)
-    }
-
-    let passwordTitle = document.querySelector("div.login div.form-div-title.password");
-    while ( passwordTitle.childNodes.length > 2 ) {
-        passwordTitle.removeChild(passwordTitle.lastChild)
-    }
-    
     // feedback error message when one of the inputs is empty
-    for ( let input of [emailInput, passwordInput] ) {
+    for (const input of document.querySelectorAll("div.login input")) {
         if  (input.value === "") {
             input.setAttribute("placeholder", "此欄位不可空白");
             input.style.border = "2px solid rgb(255, 197, 197)";
@@ -143,36 +143,55 @@ document.querySelector("button.login").addEventListener("click", async() => {
         }
     }
 
-    // check if user has signed up already by email
-    // check if password is correct
-    try {
-        let response = await fetch("/api/member/auth", {
-            method: "PUT",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                email: emailInput.value,
-                password: passwordInput.value
-            })
-        });
-
-        let result = await response.json();
-        console.log(result);
-
-        if (response.status === 400) {
-            let msg = result.message;
-            if ( msg.includes("電子信件") ) {
-                RenderErrorMessage(document.querySelector(".login .email"), result.message);
-                return;
-            }
-            else{
-                RenderErrorMessage(document.querySelector(".login .password"), result.message);
-                return;
-            }
+    // check email and password to log in
+    Login(
+        document.querySelector("div.login input[name=email]").value, 
+        document.querySelector("div.login input[name=password]").value)
+    .then(loginResult => {
+        RenderResponse(".member-response", 0, true);
+        // verification failed
+        if (loginResult.error){
+            const titleDivToAddErrorMsg = loginResult.message.includes("電子信件") ? 
+                document.querySelector(".login .email") : document.querySelector(".login .password");
+            RenderErrorMessage(titleDivToAddErrorMsg, loginResult.message);
+            return
         }
-        
-        let jwt = result.token;
-        window.localStorage.setItem("token", jwt);
+        // verification passed
+        window.localStorage.setItem("token", loginResult.token);
         window.location.replace("/main");
-    }
-    catch(error) {console.log(error)}
+    })
+    .catch(error => {console.log(error)})
+
+    // try {
+    //     let response = await fetch("/api/member/auth", {
+    //         method: "PUT",
+    //         headers: {"Content-Type": "application/json"},
+    //         body: JSON.stringify({
+    //             email: document.querySelector("div.login input[name=email]").value,
+    //             password: document.querySelector("div.login input[name=password]").value
+    //         })
+    //     });
+
+    //     let result = await response.json();
+    //     console.log(result);
+
+    //     if (response.status === 400) {
+    //         RenderResponse(".member-response", 0, true);
+    //         let msg = result.message;
+    //         if ( msg.includes("電子信件") ) {
+    //             RenderErrorMessage(document.querySelector(".login .email"), result.message);
+    //             return;
+    //         }
+    //         else{
+    //             RenderErrorMessage(document.querySelector(".login .password"), result.message);
+    //             return;
+    //         }
+    //     }
+        
+    //     RenderResponse(".member-response", 0, true);
+    //     let jwt = result.token;
+    //     window.localStorage.setItem("token", jwt);
+    //     window.location.replace("/main");
+    // }
+    // catch(error) {console.log(error)}
 });
