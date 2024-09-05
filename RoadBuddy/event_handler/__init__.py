e9a5bddf33622dbf6448d883fc7b6931b2871f52
KJeiii <pleasemailtokj@gmail.com>
2024-09-05 16:05:02 +0800

    remove global variables, user_info, sid_reference, my_sid

--- RoadBuddy/event_handler/__init__.py
+++ RoadBuddy/event_handler/__init__.py
@@ -1,42 +1,10 @@
 from RoadBuddy.event_handler.util import Online_users
 
 online_users = Online_users()
-
-# user_info = {}
-# sid_reference = {}
 rooms_info = {}
 own_coords = []
-my_sid = None
-
 
 # structure of dictionary
-
-# user_info = {
-#   user_id-1 (int) : {
-#       sid: str,
-#       username: str,
-#       email: str,
-#       team_id: str,
-#       image_url: str,
-#       coordination: [{latitude: , longitude: },{latitude: , longitude: }]
-#   },
-#   user_id-2 (int) : {
-#       sid: str,
-#       username: str,
-#       email: str,
-#       team_id: str,
-#       image_url: str,
-#       coordination: [{latitude: , longitude: },{latitude: , longitude: }]
-#   }
-# }
-
-
-# sid_reference = {
-#   sid-1 : user_id-1,
-#   sid-2 : user_id-2,
-# }
-
-
 # rooms_info = {
 #   room_id-1: {
 #       "owner_sid": str,
