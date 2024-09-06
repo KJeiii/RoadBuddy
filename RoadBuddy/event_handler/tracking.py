0f6351b49af6b27e67550167cb3cd1dcf0c85998
KJeiii <pleasemailtokj@gmail.com>
2024-09-06 21:45:19 +0800

    wrap up codes about Roadbuddy.event_handler.rooms_info with instance of online_teams and its methods

--- RoadBuddy/event_handler/tracking.py
+++ RoadBuddy/event_handler/tracking.py
@@ -1,6 +1,5 @@
-from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from flask_socketio import emit
 from RoadBuddy import socketio
-from flask import request, session
 import RoadBuddy.event_handler
 
 @socketio.on("position")
@@ -8,10 +7,12 @@ def position(new_movement):
     sid = new_movement.get("sid")
     new_coordination = new_movement.get("coordination")
     team_id = new_movement.get("team_id")
-    is_user_in_team = sid in RoadBuddy.event_handler.rooms_info[team_id]["partners"].keys()
-
+    user_id = RoadBuddy.event_handler.online_users.get_user_id(sid)
+    is_user_in_team = RoadBuddy.event_handler.online_users.get_user_information(user_id).get("team_id") == team_id
+    
     if is_user_in_team:
-        RoadBuddy.event_handler.rooms_info[team_id]["partners"][sid]["coordination"] = new_coordination
-        emit("movingPostion", RoadBuddy.event_handler.rooms_info[team_id]["partners"], to=team_id)
+        RoadBuddy.event_handler.online_teams.update_partner_coordination(
+            team_id, sid, new_coordination.get("latitude"), new_coordination.get("longitude"))
+        emit("movingPostion", RoadBuddy.event_handler.online_teams.get_all_partner_information(team_id), to=team_id)
     
 
