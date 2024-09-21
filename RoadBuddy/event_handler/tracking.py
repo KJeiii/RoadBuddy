1e65d7d6e801f346305c036d6b1ec69c4300c72e
KJeiii <pleasemailtokj@gmail.com>
2024-09-21 15:12:11 +0800

    fix running get method of NoneType: checking if user is traveling before parsing team_id

--- RoadBuddy/event_handler/tracking.py
+++ RoadBuddy/event_handler/tracking.py
@@ -8,11 +8,11 @@ def position(new_movement):
     new_coordination = new_movement.get("coordination")
     team_id = new_movement.get("team_id")
     user_id = RoadBuddy.event_handler.online_users.get_user_id(sid)
-    is_user_in_team = RoadBuddy.event_handler.online_users.get_user_information(user_id).get("team_id") == team_id
-    
-    if is_user_in_team:
-        RoadBuddy.event_handler.online_teams.update_partner_coordination(
-            team_id, sid, new_coordination.get("latitude"), new_coordination.get("longitude"))
-        emit("movingPostion", RoadBuddy.event_handler.online_teams.get_all_partner_information(team_id), to=team_id)
+    if RoadBuddy.event_handler.online_users.is_user_traveling(user_id):
+        is_user_in_team = RoadBuddy.event_handler.online_users.get_user_information(user_id).get("team_id") == team_id
+        if is_user_in_team:
+            RoadBuddy.event_handler.online_teams.update_partner_coordination(
+                team_id, sid, new_coordination.get("latitude"), new_coordination.get("longitude"))
+            emit("movingPostion", RoadBuddy.event_handler.online_teams.get_all_partner_information(team_id), to=team_id)
     
 
